761ab10e799a49ebdb157ebe71baa783da282064
Marais Rossouw <me@maraisr.com>
2019-03-15 10:56:17 +1000

    feat(TextContainer): initial impl of a TextContainer cmp

---
+++ lib/components/TextContainer/stories.tsx
@@ -0,0 +1,61 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { Button, EButtonSize, EButtonVariant } from '../Button';
+import { Heading } from '../Heading';
+import { Text } from '../Text';
+import { TextContainer } from './TextContainer';
+
+storiesOf('Components|Type/TextContainer', module)
+	.addDecorator(story => <div style={{ maxWidth: 512 }}>{story()}</div>)
+	.add('default', () => (
+		<TextContainer>
+			<Heading>Choose a credit pack</Heading>
+			<Text>
+				To get started, choose a credit pack that will used for Auto
+				Top-Up.
+			</Text>
+		</TextContainer>
+	))
+	.add('with interaction', () => (
+		<TextContainer
+			heading={
+				<>
+					<Heading>Reviews</Heading>
+					<Button
+						variant={EButtonVariant.Secondary}
+						size={EButtonSize.Small}>
+						Edit
+					</Button>
+				</>
+			}>
+			<Text>All of our reviews are from verified customers.</Text>
+		</TextContainer>
+	))
+	.add('with long title', () => (
+		<TextContainer
+			heading={
+				<>
+					<Heading>Setup your personal settings</Heading>
+					<Button
+						variant={EButtonVariant.Secondary}
+						size={EButtonSize.Small}>
+						Edit
+					</Button>
+				</>
+			}>
+			<Text>All of our reviews are from verified customers.</Text>
+		</TextContainer>
+	))
+	.add('with no body text', () => (
+		<TextContainer>
+			<Heading>Choose a credit pack</Heading>
+		</TextContainer>
+	))
+	.add('with no title text', () => (
+		<TextContainer>
+			<Text>
+				To get started, choose a credit pack that will used for Auto
+				Top-Up.
+			</Text>
+		</TextContainer>
+	));
