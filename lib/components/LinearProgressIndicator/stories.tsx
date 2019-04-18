3c39a0dced04fbf43695824c7b5c384a53872285
Amir Zahedi <amir.zahedi@gmail.com>
2019-04-18 13:39:11 +1000

    fix: Fixes broken view of the progress bar story

--- lib/components/LinearProgressIndicator/stories.tsx
+++ lib/components/LinearProgressIndicator/stories.tsx
@@ -5,6 +5,6 @@ import { LinearProgressIndicator } from '.';
 
 storiesOf('Components|Progress/Linear', module)
 	.addDecorator(story => (
-		<div style={{ width: '100vw', height: '100vh' }}>{story()}</div>
+		<div style={{ width: '100%', height: '100%' }}>{story()}</div>
 	))
 	.add('default', () => <LinearProgressIndicator />);
