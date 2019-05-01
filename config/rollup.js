89cb2bc8374ded3798e2095fa847fae661810259
Marais Rossouw <me@maraisr.com>
2019-05-01 14:52:56 +1000

    feat(ci): Only produce 1 css file

--- config/rollup.js
+++ config/rollup.js
@@ -2,13 +2,14 @@ const { dependencies, peerDependencies } = require('../package.json');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const typescript = require('rollup-plugin-typescript2');
 const postcss = require('rollup-plugin-postcss');
+const { join } = require('path');
 
 const externalPackages = [
 	...Object.keys(dependencies || {}),
 	...Object.keys(peerDependencies || {}),
 ];
 
-function createRollupConfig({ entry: input }) {
+function createRollupConfig({ entry: input, outputPath }) {
 	return {
 		input,
 
@@ -25,12 +26,26 @@ function createRollupConfig({ entry: input }) {
 			}),
 
 			postcss({
-				extract: true,
+				extract: join(outputPath, 'overdrive.css'),
 				inject: false,
 				modules: {
 					camelCase: true,
 					generateScopedName: '_[hash:base64:5]',
 				},
+				plugins: [
+					require('postcss-flexbugs-fixes'),
+					require('postcss-preset-env')({
+						autoprefixer: {
+							flexbox: 'no-2009',
+						},
+						stage: 3,
+					}),
+					require('cssnano')({
+						discardComments: {
+							removeAll: true,
+						},
+					}),
+				],
 			}),
 
 			typescript(),
--- scripts/build.js
+++ scripts/build.js
@@ -21,6 +21,7 @@ async function run() {
 	const bundle = await rollup(
 		createRollupConfig({
 			entry,
+			outputPath: distFolder,
 		})
 	);
 
