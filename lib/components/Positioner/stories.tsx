33057194f6a3efcc237e6b94cc152e879552f86f
Marais Rossouw <me@maraisr.com>
2019-06-04 08:41:41 +1000

    chore: Added a story that helps with scrolling

--- lib/components/Positioner/stories.tsx
+++ lib/components/Positioner/stories.tsx
@@ -1,6 +1,6 @@
 import { select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { createRef, useCallback, useRef, useState } from 'react';
 import { Box } from '../Box';
 import { EAlignment } from './alignment';
 import { Positioner } from './Positioner';
@@ -8,32 +8,83 @@ import { Positioner } from './Positioner';
 const alignmentPicker = () =>
 	select('Alignment', EAlignment, EAlignment.BOTTOM_LEFT);
 
-storiesOf('Utility|Positioner', module).add('default', () => {
-	const Impl = () => {
-		const triggerRef = useRef();
-		const [isOpen, setIsOpen] = useState(true);
+storiesOf('Utility|Positioner', module)
+	.addParameters({
+		chromatic: { delay: 300 },
+	})
+	.add('default', () => {
+		const Impl = () => {
+			const triggerRef = useRef();
+			const [isOpen, setIsOpen] = useState(true);
 
-		return (
-			<div>
-				<button
-					ref={triggerRef}
-					onClick={useCallback(() => setIsOpen(!isOpen), [isOpen])}>
-					Open me
-				</button>
-				<Positioner
-					isOpen={isOpen}
-					triggerRef={triggerRef}
-					alignment={alignmentPicker()}>
-					<Box distance={1}>
-						<div style={{ padding: 'var(--global--space--2)' }}>
-							Hello im from the consumer:{' '}
-							{Math.ceil(Math.random() * 100)}
-						</div>
-					</Box>
-				</Positioner>
-			</div>
-		);
-	};
+			return (
+				<div>
+					<button
+						ref={triggerRef}
+						onClick={useCallback(() => setIsOpen(!isOpen), [
+							isOpen,
+						])}>
+						Open me
+					</button>
+					<Positioner
+						isOpen={isOpen}
+						triggerRef={triggerRef}
+						alignment={alignmentPicker()}>
+						<Box distance={1}>
+							<div style={{ padding: 'var(--global--space--2)' }}>
+								Hello im from the consumer:{' '}
+								{Math.ceil(Math.random() * 100)}
+							</div>
+						</Box>
+					</Positioner>
+				</div>
+			);
+		};
 
-	return <Impl />;
-});
+		return <Impl />;
+	})
+	.add(
+		'illustrate a scroll',
+		() => {
+			const triggerRef = createRef();
+
+			return (
+				<div
+					style={{
+						height: '100%',
+						width: '100%',
+						overflow: 'scroll',
+					}}>
+					<div
+						style={{
+							height: 'calc(100vh*5)',
+							width: 'calc(100vw*5)',
+						}}>
+						<button
+							style={{
+								marginTop: 'calc((100vh*5) / 2)',
+								marginLeft: 'calc((100vw*5) / 2)',
+							}}
+							ref={triggerRef}>
+							I'm the trigger
+						</button>
+						<Positioner
+							isOpen={true}
+							triggerRef={triggerRef}
+							alignment={alignmentPicker()}>
+							<Box distance={1}>
+								<div
+									style={{
+										padding: 'var(--global--space--2)',
+									}}>
+									Hello im from the consumer:{' '}
+									{Math.ceil(Math.random() * 100)}
+								</div>
+							</Box>
+						</Positioner>
+					</div>
+				</div>
+			);
+		},
+		{ chromatic: { disable: true } }
+	);
