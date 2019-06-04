99ea1f927f4185403ca5e6eff801654bb14214d9
Marais Rossouw <me@maraisr.com>
2019-06-04 11:18:10 +1000

    feat(Flyout): Initial impl of the Flyout component (#93)

---
+++ lib/components/Flyout/stories.tsx
@@ -0,0 +1,52 @@
+import { select } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import React, { useCallback, useRef, useState } from 'react';
+import { Flyout } from '.';
+import { Button, EButtonSize, EButtonVariant } from '../Button';
+import { EPositionerAlignment } from '../Positioner';
+import { TextInput } from '../TextInput';
+
+const alignmentPicker = () =>
+	select('Alignment', EPositionerAlignment, EPositionerAlignment.BOTTOM_LEFT);
+
+storiesOf('Components|Flyout', module).add('default', () => {
+	const Impl = () => {
+		const triggerRef = useRef();
+		const [isOpen, setIsOpen] = useState(true);
+		const closer = useCallback(() => setIsOpen(false), []);
+		const toggle = useCallback(() => setIsOpen(!isOpen), [isOpen]);
+
+		return (
+			<>
+				<Button ref={triggerRef} onClick={toggle}>
+					some trigger
+				</Button>
+				<Flyout
+					triggerRef={triggerRef}
+					alignment={alignmentPicker()}
+					isOpen={isOpen}
+					onRequestClose={closer}>
+					<div
+						style={{
+							display: 'grid',
+							gridTemplateRows: 'repeat(2, auto)',
+							gridGap: 'var(--global--space--4)',
+							padding: 'var(--global--space--4)',
+						}}>
+						<TextInput name="example" placeholder="example" />
+						<div>
+							<Button
+								size={EButtonSize.Small}
+								variant={EButtonVariant.Primary}
+								onClick={closer}>
+								Save
+							</Button>
+						</div>
+					</div>
+				</Flyout>
+			</>
+		);
+	};
+
+	return <Impl />;
+});
