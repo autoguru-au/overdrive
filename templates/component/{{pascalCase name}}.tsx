fcfe7e2609ff9e318fa5ab96383a9464787267bb
Marais Rossouw <me@maraisr.com>
2019-06-05 09:26:49 +1000

    chore: Updated plop file ext's

--- templates/component/{{pascalCase name}}.tsx
+++ templates/component/{{pascalCase name}}.tsx
@@ -5,6 +5,8 @@ import styles from './style.scss';
 
 interface IProps {}
 
-const {{pascalCase name}}Component:FunctionComponent<IProps> = () => <p className={clsx(styles.root)}>New {{name}}!</p>;
+const {{pascalCase name}}Component:FunctionComponent<IProps> = () => <p className={clsx(styles.root)}>
+	New component {{name}}! 🎉 Edit lib/components/{{pascalCase name}}/{{pascalCase name}}.tsx
+</p>;
 
 export const {{pascalCase name}} = memo({{pascalCase name}}Component);
