d3a06fb709039df8bf649fd5266da2691a4e55d7
Marais Rossouw <me@maraisr.com>
2019-07-03 14:57:55 +1000

    feat(TextContainer): Adds className as a forwarded prop

--- lib/components/TextContainer/TextContainer.tsx
+++ lib/components/TextContainer/TextContainer.tsx
@@ -1,15 +1,18 @@
 import React, { FunctionComponent, ReactElement } from 'react';
 import styles from './style.scss';
+import clsx from 'clsx';
 
 export interface Props {
 	heading?: ReactElement | null;
+	className?: string;
 }
 
 export const TextContainer: FunctionComponent<Props> = ({
 	heading = null,
+	className = '',
 	children,
 }) => (
-	<article className={styles.root}>
+	<article className={clsx(styles.root, className)}>
 		{Boolean(heading) && <header>{heading}</header>}
 		{children}
 	</article>
