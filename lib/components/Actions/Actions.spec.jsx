de46c867baeb88652c90d7d7a5765f5ee2757ca2
Marais Rossouw <me@maraisr.com>
2020-03-20 11:02:29 +1000

    feat: Actions now filters out undefined or null children

--- lib/components/Actions/Actions.spec.jsx
+++ lib/components/Actions/Actions.spec.jsx
@@ -11,10 +11,4 @@ describe('<Actions />', () => {
 	it('should match the snapshot', () => {
 		expect(render(<Actions />).container.firstChild).toMatchSnapshot();
 	});
-
-	it.skip('should pass className down', () => {
-		expect(
-			render(<Actions className="test" />).container.firstChild,
-		).toHaveClass('test');
-	});
 });
--- lib/components/Actions/Actions.tsx
+++ lib/components/Actions/Actions.tsx
@@ -12,24 +12,19 @@ interface Props
 	extends Partial<
 		Pick<ComponentProps<typeof Columns>, 'noWrap' | 'wrappingDirection'>
 	> {
-	className?: string;
 	children: ReactElement | ReactElement[];
 }
 
-// TODO: Has the potential to break on fragments
 export const Actions: FunctionComponent<Props> = ({
-	className,
 	children,
 	noWrap,
 	wrappingDirection,
 }) => (
-	<Columns
-		space="3"
-		noWrap={noWrap}
-		wrappingDirection={wrappingDirection}
-		className={className}>
-		{Children.map(children, (child, idx) => (
-			<Column key={idx}>{child}</Column>
-		))}
+	<Columns space="3" noWrap={noWrap} wrappingDirection={wrappingDirection}>
+		{Children.map(children, (child, idx) =>
+			child !== undefined && child !== null ? (
+				<Column key={idx}>{child}</Column>
+			) : null,
+		)}
 	</Columns>
 );
