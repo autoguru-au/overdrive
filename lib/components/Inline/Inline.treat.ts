9aaa3ac4aa9b783b9c289be789fb11fb8b1d82ac
Marais Rossouw <me@maraisr.com>
2020-03-27 11:39:39 +1000

    feat(Inline): Supports alignY responsiveProp

--- lib/components/Inline/Inline.treat.ts
+++ lib/components/Inline/Inline.treat.ts
@@ -1,7 +1,17 @@
 import { style } from 'treat';
 
+import { makeResponsiveStyle } from '../../utils';
+
 export const root = style({
 	display: 'flex',
 	justifyContent: 'flex-start',
 	flexWrap: 'wrap',
 });
+
+const alignRules = {
+	top: 'flex-start',
+	center: 'center',
+	bottom: 'flex-end',
+};
+
+export const align = makeResponsiveStyle(() => alignRules, 'alignItems');
