5ce3b0f3bcd8031a30f7d204d4ee948762d4acf5
Amir Zahedi <amir.zahedi@gmail.com>
2020-04-08 16:31:34 +1000

    feat: Fixes columns component not filling its parent

--- lib/hooks/useNegativeMargin/useNegativeMarginLeft.treat.ts
+++ lib/hooks/useNegativeMargin/useNegativeMarginLeft.treat.ts
@@ -2,5 +2,8 @@ import { makeResponsiveStyle } from '../../utils';
 
 export const negativeMarginLeft = makeResponsiveStyle(
 	(theme) => theme.space,
-	(value) => ({ marginLeft: `calc(-1 * ${value})` }),
+	(value) => ({
+		marginLeft: `calc(-1 * ${value})`,
+		width: `calc(100% + ${value})`,
+	}),
 );
