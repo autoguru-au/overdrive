6979fee07e20d91db745ef348d0f74cb23fc6060
Marais Rossouw <me@maraisr.com>
2020-06-02 14:15:52 +1000

    chore: Adds icons to playroom

--- .playroom/frame.tsx
+++ .playroom/frame.tsx
@@ -2,7 +2,11 @@ import '../lib/reset/font-face.css';
 import '../lib/reset';
 
 import * as React from 'react';
+import * as Icons from '@autoguru/icons';
 import { OverdriveProvider } from '../lib/components/OverdriveProvider';
+
+// @ts-ignore
+global.Icons = Icons;
 
 export default ({ theme, children }) =>
 	<OverdriveProvider theme={theme}>
