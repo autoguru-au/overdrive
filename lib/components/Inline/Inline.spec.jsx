04281e774f98e46d45d7a762cfa5ce836c191ad7
Marais Rossouw <me@maraisr.com>
2020-06-04 15:47:16 +1000

    chore: Added some unit tests

--- lib/components/Inline/Inline.spec.jsx
+++ lib/components/Inline/Inline.spec.jsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { Inline } from '.';
+import { Text } from '../Typography';
+
+const TestComponent = () => <div>divider component</div>;
 
 describe('<Inline />', () => {
 	it('should not throw', () => {
 		expect(() => render(<Inline />)).not.toThrow();
 	});
 
-	it('should match the snapshot', () => {
-		expect(render(<Inline />).container.firstChild).toMatchSnapshot();
+	it.each([
+		['default', true],
+		['no', false],
+		['custom element', <div>divider</div>],
+		['custom component', TestComponent],
+	])('should match snapshot for %s dividers', (value) => {
+		const { container } = render(
+			<Inline dividers={value}>
+				<Text>A</Text>
+				<Text>B</Text>
+				<Text>C</Text>
+			</Inline>,
+		);
+
+		expect(container.firstChild).toMatchSnapshot();
 	});
 });
