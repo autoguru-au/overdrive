3d88e35d309cc86ad5a19754ca6f320a2c15d37d
Amir Zahedi <amir@autoguru.com.au>
2020-07-22 11:06:29 +1000

    refactor: Change SimplePagination to use Inline

--- packages/overdrive/lib/components/SimplePagination/SimplePagination.tsx
+++ packages/overdrive/lib/components/SimplePagination/SimplePagination.tsx
@@ -1,13 +1,10 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@autoguru/icons';
-import clsx from 'clsx';
 import * as React from 'react';
 import { memo, NamedExoticComponent } from 'react';
-import { useStyles } from 'react-treat';
 
-import { Box } from '../Box';
 import { Button } from '../Button';
 import { Icon } from '../Icon';
-import * as styleRefs from './SimplePagination.treat';
+import { Inline } from '../Inline';
 
 export enum EChangeDirection {
 	Previous = 'previous',
@@ -17,7 +14,6 @@ export enum EChangeDirection {
 type TOnChangeEventHandler = (event: EChangeDirection) => void;
 
 export interface Props {
-	className?: string;
 	hasNext?: boolean;
 	hasPrevious?: boolean;
 	onChange?: TOnChangeEventHandler;
@@ -25,39 +21,24 @@ export interface Props {
 
 export const SimplePagination: NamedExoticComponent<Props> = memo(
 	({
-		className = '',
 		hasNext = false,
 		hasPrevious = false,
 		onChange = () => undefined,
 	}) => {
-		const styles = useStyles(styleRefs);
-		const cls = clsx([styles.pagination, className]);
-		const chevronLeftCls = clsx(styles.pagination, styles.chevron.default, {
-			[styles.chevron.disabled]: !hasPrevious,
-		});
-		const chevronRightCls = clsx(
-			styles.pagination,
-			styles.chevron.default,
-			{
-				[styles.chevron.disabled]: !hasNext,
-			},
-		);
 		const handleClick = (direction: EChangeDirection) => () => {
 			onChange(direction);
 		};
 
 		return (
-			<Box is="nav" className={cls} aria-label="pagination">
+			<Inline is="nav" space='6' aria-label="pagination">
 				<Button
 					rounded
 					disabled={!hasPrevious}
 					size="small"
 					variant="secondary"
-					className={chevronLeftCls}
 					aria-label="previous page"
 					onClick={handleClick(EChangeDirection.Previous)}>
 					<Icon
-						className={styles.icon.default}
 						size="medium"
 						icon={ChevronLeftIcon}
 					/>
@@ -67,16 +48,14 @@ export const SimplePagination: NamedExoticComponent<Props> = memo(
 					disabled={!hasNext}
 					size="small"
 					variant="secondary"
-					className={chevronRightCls}
 					aria-label="next page"
 					onClick={handleClick(EChangeDirection.Next)}>
 					<Icon
-						className={styles.icon.default}
 						size="medium"
 						icon={ChevronRightIcon}
 					/>
 				</Button>
-			</Box>
+			</Inline>
 		);
 	},
 );
