c5857653bfe971fe2fbdb13936e95b61607830a3
Marais Rossouw <me@maraisr.com>
2020-08-25 12:26:30 +1000

    feat(Heading): Accepts an id prop

---
+++ .changeset/many-cameras-learn.md
@@ -0,0 +1,6 @@
+---
+'@autoguru/overdrive': patch
+---
+
+**Heading:** Adds an `id` prop which is applied directly to the underlying
+element
--- packages/overdrive/lib/components/Heading/Heading.tsx
+++ packages/overdrive/lib/components/Heading/Heading.tsx
@@ -1,38 +1,39 @@
 import * as React from 'react';
-import { FunctionComponent } from 'react';
+import { ComponentProps, FunctionComponent } from 'react';
 import type { Theme } from 'treat/theme';
 
 import { Box } from '../Box';
 import type { TextStyleProps } from '../Text';
 import { useTextStyles } from '../Text';
 
-export interface Props extends TextStyleProps {
+export interface Props
+	extends TextStyleProps,
+		Pick<ComponentProps<typeof Box>, 'id'> {
 	className?: string;
 	is?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 	colour?: Exclude<keyof Theme['typography']['colour'], 'muted'>;
 }
 
-const sizeScaleDefaults: Map<
-	string,
-	keyof Theme['typography']['size']
-> = new Map([
-	['h1', '8'],
-	['h2', '7'],
-	['h3', '6'],
-	['h4', '4'],
-	['h5', '3'],
-	['h6', '2'],
-]);
+const sizeScaleDefaults = {
+	h1: '8',
+	h2: '7',
+	h3: '6',
+	h4: '4',
+	h5: '3',
+	h6: '2',
+} as const;
 
 export const Heading: FunctionComponent<Props> = ({
-	children,
-	className = '',
 	is = 'h1',
-	colour = 'dark',
 	align,
-	size = sizeScaleDefaults.get(is),
+	colour = 'dark',
+	size = sizeScaleDefaults[is],
+	id,
+	className = '',
+	children,
 }) => (
 	<Box
+		id={id}
 		is={is}
 		className={[
 			useTextStyles({
