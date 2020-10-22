347cf130ab3d8fe78d92acf0b08253056c442672
Amir Zahedi <amir@autoguru.com.au>
2020-10-22 12:06:25 +1000

    feat: Increases select input click area to cover full component width

--- packages/overdrive/lib/components/SelectInput/SelectInput.tsx
+++ packages/overdrive/lib/components/SelectInput/SelectInput.tsx
@@ -2,37 +2,56 @@ import { ChevronDownIcon } from '@autoguru/icons';
 import * as React from 'react';
 import { ReactNode } from 'react';
 
-import { Box, useBoxStyles } from '../Box';
+import { Box } from '../Box';
 import { Icon } from '../Icon';
 import { withEnhancedInput } from '../private/InputBase';
+import { useStyles } from 'react-treat';
+import * as styleRefs from './SelectInput.treat';
 
 export const SelectInput = withEnhancedInput<
 	{
 		children: ReactNode[];
+		c
 	},
 	HTMLSelectElement
 >(
-	({ field, eventHandlers, suffixed, prefixed, validation, ...rest }) => (
-		<Box
-			display="flex"
-			flexWrap="nowrap"
-			alignItems="center"
-			justifyContent="center">
+	({ field, eventHandlers, suffixed, prefixed, validation, ...rest }) => {
+
+		const styles = useStyles(styleRefs);
+		return (
 			<Box
-				is="select"
-				flexGrow={1}
-				{...eventHandlers}
-				{...field}
-				{...rest}
-				autoComplete="off"
-			/>
-			<Icon
-				size="medium"
-				icon={ChevronDownIcon}
-				className={useBoxStyles({ marginRight: '4', flexShrink: 0 })}
-			/>
-		</Box>
-	),
+				display="flex"
+				flexWrap="nowrap"
+				alignItems="center"
+				justifyContent="center"
+				position="relative">
+				<Box
+					is="select"
+					flexGrow={1}
+					{...eventHandlers}
+					{...field}
+					{...rest}
+					className={[styles.input, styles.paddedInput, field.className]}
+					autoComplete="off"
+				/>
+				<Box
+					className={styles.arrow}
+					display="flex"
+					alignItems="center"
+					height="full"
+					marginRight= '4'
+					flexShrink={0}
+					pointerEvents='none'
+					position='absolute'>
+					<Icon
+						size="medium"
+						icon={ChevronDownIcon}
+					/>
+				</Box>
+
+			</Box>
+		);
+	},
 	{
 		primitiveType: 'select',
 		withSuffixIcon: false,
