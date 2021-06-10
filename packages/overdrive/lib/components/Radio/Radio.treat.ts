7ac808826d2a27535160e0fe0ded6014bb017ba7
Amir Zahedi <amir.zahedi@gmail.com>
2021-06-11 08:46:13 +1000

    refactor: Gmut-free radio

--- packages/overdrive/lib/components/Radio/Radio.treat.ts
+++ packages/overdrive/lib/components/Radio/Radio.treat.ts
@@ -1,25 +1,25 @@
 import { styleMap } from 'treat';
 
-export const circle = styleMap((theme) => ({
+export const circle = styleMap(({ animation, space, typography }) => ({
 	default: {
-		transition: `border-color 0.2s ${theme.animation.easing.decelerate} 0s`,
+		transition: `border-color 0.2s ${animation.easing.decelerate} 0s`,
 	},
 	outer: {
-		top: theme.space['3'],
-		left: theme.space['3'],
-		width: theme.space['4'],
-		height: theme.space['4'],
+		top: space['3'],
+		left: space['3'],
+		width: space['4'],
+		height: space['4'],
 	},
 	inner: {
-		top: `calc(${theme.space['3']} + (${theme.space['4']} - ${theme.space['2']}) * 0.5)`,
-		left: `calc(${theme.space['3']} + (${theme.space['4']} - ${theme.space['2']}) * 0.5)`,
-		width: theme.space['2'],
-		height: theme.space['2'],
+		top: `calc(${space['3']} + (${space['4']} - ${space['2']}) * 0.5)`,
+		left: `calc(${space['3']} + (${space['4']} - ${space['2']}) * 0.5)`,
+		width: space['2'],
+		height: space['2'],
 	},
 	selectedInner: {
-		borderWidth: `calc(${theme.space['2']} / 2)`,
+		borderWidth: `calc(${space['2']} / 2)`,
 	},
 	selected: {
-		borderColor: theme.colours.gamut.green600,
+		borderColor: typography.colour.primary,
 	},
 }));
