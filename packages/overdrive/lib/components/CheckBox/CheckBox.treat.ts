0dd9d03e7cd5609fd320bfb96f440f8c76466e68
Amir Zahedi <amir.zahedi@gmail.com>
2021-06-10 15:34:23 +1000

    refactor: Gmut-free checkbox

--- packages/overdrive/lib/components/CheckBox/CheckBox.treat.ts
+++ packages/overdrive/lib/components/CheckBox/CheckBox.treat.ts
@@ -8,18 +8,18 @@ export const icon = style((theme) => ({
 	height: theme.space['4'],
 }));
 
-export const base = styleMap((theme) => ({
+export const base = styleMap(({ space, animation, colours }) => ({
 	default: {
 		zIndex: 0,
 		borderRadius: '2px',
-		top: theme.space['3'],
-		left: theme.space['3'],
-		width: theme.space['4'],
-		height: theme.space['4'],
-		transition: `border-color 0.2s ${theme.animation.easing.decelerate} 0s, background-color 0.2s ${theme.animation.easing.decelerate} 0s`,
+		top: space['3'],
+		left: space['3'],
+		width: space['4'],
+		height: space['4'],
+		transition: `border-color 0.2s ${animation.easing.decelerate} 0s, background-color 0.2s ${animation.easing.decelerate} 0s`,
 	},
 
 	selected: {
-		borderColor: theme.colours.gamut.green600,
+		borderColor: colours.intent.primary.background,
 	},
 }));
