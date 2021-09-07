7f0447277bbae0a4a8ab5255b57c8ab3db01f153
Amir Zahedi <amir.zahedi@gmail.com>
2021-09-07 11:50:02 +1000

    chore: Exports useAttachedBoxes for libs index

--- packages/overdrive/lib/index.ts
+++ packages/overdrive/lib/index.ts
@@ -2,4 +2,5 @@ export * from './components';
 
 export { useMedia } from './hooks/useMedia';
 export { useResponsiveValue } from './hooks/useResponsiveValue';
+export { useAttachedBoxes } from './hooks/useAttachedBoxes';
 export { useId, arrayRingLookup } from './utils';
