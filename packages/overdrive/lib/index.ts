6c3115c76bcfd67358cfbe3e5938982806d851e3
Amir Zahedi <amir.zahedi@gmail.com>
2021-09-23 10:10:40 +1000

    chore: Adds exports

--- packages/overdrive/lib/index.ts
+++ packages/overdrive/lib/index.ts
@@ -4,3 +4,7 @@ export { useMedia } from './hooks/useMedia';
 export { useResponsiveValue } from './hooks/useResponsiveValue';
 export { useAttachedBoxes } from './hooks/useAttachedBoxes';
 export { useId, arrayRingLookup } from './utils';
+export {
+	mapTokenToProperty
+}  from './utils/mapTokenToProperty';
+export {makeResponsiveStyle, resolveResponsiveStyle} from './utils/responsiveProps';
