c410458dc3735572d1d241f425df2a539be06c82
Amir Zahedi <amir.zahedi@gmail.com>
2021-10-14 14:44:43 +1000

    wip: OD Upgrades

--- packages/overdrive/lib/components/Switch/stories.tsx
+++ packages/overdrive/lib/components/Switch/stories.tsx
@@ -1,22 +1,34 @@
 import { action } from '@storybook/addon-actions';
-import { boolean } from '@storybook/addon-knobs';
 import * as React from 'react';
 
 import { Switch } from '.';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Stack } from '../Stack';
+import { Text } from '../Text';
+import { SimplePagination } from '../SimplePagination';
 
 export default {
 	title: 'Components/Switch',
 	component: Switch,
+}as ComponentMeta<typeof Switch>;
+
+const Template: ComponentStory<typeof Switch> = (args) => (
+	<Switch {...args}/>
+);
+
+
+const standardProps = {
+	disabled: false,
+	toggled: false,
+	onChange: action('onChange'),
 };
 
-export const standard = () => (
-	<Switch
-		disabled={boolean('disabled', false)}
-		toggled={boolean('toggled', false)}
-		onChange={action('onChange')}
-	/>
-);
+export const untoggled: ComponentStory<typeof Switch> = Template.bind(standardProps);
+untoggled.args = standardProps;
+
+/*
 export const untoggled = () => <Switch toggled={false} />;
 export const toggled = () => <Switch toggled />;
 export const disabledUntoggled = () => <Switch disabled toggled={false} />;
 export const disabledToggled = () => <Switch disabled toggled />;
+*/
