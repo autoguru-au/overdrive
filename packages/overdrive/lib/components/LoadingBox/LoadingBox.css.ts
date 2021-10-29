da8a6da4b648c9cd96f2f2aa00792106c0657d9c
Amir Zahedi <amir.zahedi@gmail.com>
2021-10-29 11:53:58 +1000

    wip: OD Upgrades

---
+++ packages/overdrive/lib/components/LoadingBox/LoadingBox.css.ts
@@ -0,0 +1,27 @@
+import { keyframes, style } from '@vanilla-extract/css';
+import { vars } from '../../themes/base/vars.css';
+
+export const root = style({
+	minHeight: vars.typography.size['3'].fontSize,
+});
+
+const blinker = keyframes({
+	'0%': {
+		opacity: 0.4,
+	},
+
+	'50%': {
+		opacity: 1,
+	},
+
+	'100%': {
+		opacity: 0.4,
+	},
+});
+
+export const blinking = style({
+	animationDuration: '1.5s',
+	animationTimingFunction: vars.animation.easing.standard,
+	animationIterationCount: 'infinite',
+	animationName: blinker,
+});
--- packages/overdrive/lib/components/LoadingBox/LoadingBox.tsx
+++ packages/overdrive/lib/components/LoadingBox/LoadingBox.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { ComponentProps, FunctionComponent } from 'react';
-import { useStyles } from 'react-treat';
 
 import { Box } from '../Box';
 
-import * as styleRefs from './LoadingBox.treat';
+import * as styles from './LoadingBox.css';
 
 export interface Props extends Omit<ComponentProps<typeof Box>, 'width'> {
 	className?: string;
@@ -13,30 +12,26 @@ export interface Props extends Omit<ComponentProps<typeof Box>, 'width'> {
 }
 
 export const LoadingBox: FunctionComponent<Props> = ({
-	className = '',
-	randomWidth = false,
-	blinking = true,
-	backgroundColour = 'gray200',
-	display = 'block',
-	is = 'span',
-	...boxStyles
-}) => {
-	const styles = useStyles(styleRefs);
-
-	return (
-		<Box
-			is={is}
-			display={display}
-			width={randomWidth ? undefined : 'full'}
-			backgroundColour={backgroundColour}
-			{...boxStyles}
-			className={[styles.root, blinking && styles.blinking, className]}
-			style={{
-				width: randomWidth ? getRandomIntWidth(60, 40) : undefined,
-			}}
-		/>
-	);
-};
+														 className = '',
+														 randomWidth = false,
+														 blinking = true,
+														 backgroundColour = 'gray200',
+														 display = 'block',
+														 is = 'span',
+														 ...boxStyles
+													 }) => (
+	<Box
+		is={is}
+		display={display}
+		width={randomWidth ? undefined : 'full'}
+		backgroundColour={backgroundColour}
+		{...boxStyles}
+		className={[styles.root, blinking && styles.blinking, className]}
+		style={{
+			width: randomWidth ? getRandomIntWidth(60, 40) : undefined,
+		}}
+	/>
+)
 
 const getRandomIntWidth = (max: number, min: number) =>
 	`${Math.random() * (max - min) + min}%`;
