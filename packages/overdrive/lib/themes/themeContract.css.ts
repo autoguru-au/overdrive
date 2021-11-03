1aa76cccd91416a5376c557840a97b9a2bb500a3
Amir Zahedi <amir.zahedi@gmail.com>
2021-11-03 15:23:16 +1000

    chore: Deletes redundant theme contract

--- packages/overdrive/lib/themes/themeContract.css.ts
+++
@@ -1,6 +0,0 @@
-import { createThemeContract } from '@vanilla-extract/css';
-
-import { tokens } from './base/tokens';
-import { Tokens } from './tokens';
-
-export const contractVars = createThemeContract<Tokens>(tokens);
