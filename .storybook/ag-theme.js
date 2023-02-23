60dd824a9bc12878ae82392521508cac03b289fd
Amir Zahedi <amir.zahedi@gmail.com>
2023-02-23 08:03:13 +0800

    feat: Themes storybook

---
+++ .storybook/ag-theme.js
@@ -0,0 +1,14 @@
+import { create } from '@storybook/theming';
+
+export default create({
+	base: 'dark',
+	brandTitle: 'AutoGuru SDK Client Components',
+	brandUrl: 'https://www.autoguru.com.au/',
+	brandImage: 'https://cdn.autoguru.com.au/images/logos/otto-white.svg',
+	brandTarget: '_self',
+	colorPrimary: '#ffc001',
+	colorSecondary: '#03af83',
+	textInverseColor: '#ffc001',
+	barSelectedColor: '#ffc001',
+	appBorderRadius: 3,
+});
