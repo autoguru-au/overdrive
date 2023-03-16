4f50720137f0ba9164540935afca3d97bda7ba09
Amir Zahedi <amir.zahedi@gmail.com>
2023-03-16 13:21:20 +0800

    test: Adds tests

---
+++ packages/overdrive/lib/components/HorizontalAutoScroller/HorizontalAutoScroller.spec.jsx
@@ -0,0 +1,59 @@
+import { render } from '@testing-library/react';
+import * as React from 'react';
+
+import 'intersection-observer';// polyfill for testing
+
+import { HorizontalAutoScroller } from './';
+
+const standardProps = {
+	paused: false,
+	activePage: 0,
+};
+
+describe('<HorizontalAutoScroller />', () => {
+	it('should not throw', () => {
+		expect(() =>
+			render(<HorizontalAutoScroller {...standardProps} />),
+		).not.toThrow();
+	});
+
+	it('should not throw with many items', () => {
+		expect(() =>
+			render(
+				<HorizontalAutoScroller {...standardProps}>{
+					Array.from({ length: 40 }).map((_, index) => (
+						<span key={index} />
+					))
+				}
+				</HorizontalAutoScroller>,
+			)).not.toThrow();
+	});
+
+	it('should match the snapshot for 8 items', () => {
+		expect(
+			render(
+				<HorizontalAutoScroller {...standardProps}>
+					{
+						Array.from({ length: 8 }).map((_, index) => (
+							<span key={index} />
+						))
+					}
+				</HorizontalAutoScroller>,
+			).container.firstChild,
+		).toMatchSnapshot();
+	});
+
+	it('should match the snapshot with custom slider colour', () => {
+		expect(
+			render(
+				<HorizontalAutoScroller {...standardProps} sliderProgressColour='yellow500'>
+					{
+						Array.from({ length: 4 }).map((_, index) => (
+							<span key={index} />
+						))
+					}
+				</HorizontalAutoScroller>,
+			).container.firstChild,
+		).toMatchSnapshot();
+	});
+});
