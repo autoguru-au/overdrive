c8f6330ccdd9d76fbf544a82144c4ade9baa8b74
Amir Zahedi <amir.zahedi@gmail.com>
2023-03-28 12:29:37 +0800

    feat: Adds theme token utils (#881)

---
+++ .changeset/big-experts-battle.md
@@ -0,0 +1,5 @@
+---
+'@autoguru/overdrive': patch
+---
+
+New theme token utils to read theme token values
---
+++ packages/overdrive/lib/utils/css.ts
@@ -0,0 +1,14 @@
+const cssVarRegExp = /var\(([^)]+)\)/;
+
+export const cssVarUnwrap = (value: string) => {
+	const matches = cssVarRegExp.exec(value);
+
+	return matches ? matches[1] : value;
+};
+
+export const getThemeTokenValue = (themeClass: string, token: string): string | null => {
+	const cssVar = cssVarUnwrap(token);
+	const themedElement = document.querySelector(`.${themeClass}`);
+	if (!themedElement || !cssVar) return null;
+	return getComputedStyle(themedElement).getPropertyValue(cssVar)?.trim() || null;
+};
