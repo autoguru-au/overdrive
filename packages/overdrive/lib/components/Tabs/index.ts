3e99366730b201afa4ab8c8e0a36daf3d1c8740d
Yasmine Shakib <134252114+yshakib@users.noreply.github.com>
2024-06-24 10:54:32 +1000

    Update index.ts
    refactor: Adds space

--- packages/overdrive/lib/components/Tabs/index.ts
+++ packages/overdrive/lib/components/Tabs/index.ts
@@ -2,4 +2,4 @@ export { Tabs } from './Tabs';
 export { Tab } from './Tab';
 export { TabList } from './TabList';
 export { TabPane } from './TabPane';
-export { TabPanes } from './TabPanes';
+export { TabPanes } from './TabPanes'; 
