776d24899fcade9bee5918df8a6f71937699662f
Sergio Gimenez <36163163+sgimama@users.noreply.github.com>
2024-09-06 14:47:18 +1000

    feat: change css to adapt new design

--- packages/overdrive/lib/components/Tabs/TabList.css.ts
+++ packages/overdrive/lib/components/Tabs/TabList.css.ts
@@ -3,9 +3,6 @@ import { style, styleVariants } from '@vanilla-extract/css';
 import { themeContractVars as vars } from '../../themes/theme.css';
 
 export const root = styleVariants({
-	default: {
-		boxShadow: `inset 0 -1px 0 0 ${vars.border.colours.gray}`,
-	},
 	scroll: {
 		display: 'grid',
 		gridTemplateColumns: 'auto 1fr auto',
