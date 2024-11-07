fdc459474d3652943b62cc7f6ab6959e8be24a35
xavicosco <xavicosco@gmail.com>
2024-11-07 17:02:22 +1100

    feat: Handle paste functionality

--- packages/overdrive/lib/components/PinInput/PinInput.tsx
+++ packages/overdrive/lib/components/PinInput/PinInput.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { memo, NamedExoticComponent } from 'react';
-
+import { FunctionComponent, useRef } from 'react';
 import { Box } from '../Box';
 import { Inline } from '../Inline';
 import { TextInput } from '../TextInput';
@@ -11,7 +10,6 @@ import { Tokens } from '../../themes/tokens';
 export enum EPinInputSize {
 	Small = 'small',
 	Medium = 'medium',
-
 }
 
 export interface Props {
@@ -27,23 +25,38 @@ const labelSizeMap: Map<EPinInputSize, keyof Tokens['typography']['size']> =
 		[EPinInputSize.Medium, '4'],
 	]);
 
-export const PinInput: NamedExoticComponent<Props> = memo(
-	({
+export const PinInput: FunctionComponent<Props> = ({
 		className = '',
 		digits,
-		 value = digits,
+		value = digits,
 		size = EPinInputSize.Medium,
-	}) => (
-		<Box className={className}>
-			<Inline space={labelSizeMap.get(size)} alignY="center">
-				{Array.from({ length: digits })
-					.fill(0)
-					.map((_, index) => (
-						<TextInput key={index} className={styles.inputDigit[size]} size={size} maxLength={1}  name="digit" placeholder='' />
-					))}
-			</Inline>
-		</Box>
-	),
-);
+	}) => {
+		const handleOnPaste = (event) => {
+			const stringPasted = event.clipboardData.getData('Text');
+			//const stringPastedLength = event.clipboardData.getData('Text').length;
+
+			[...stringPasted].forEach((digit, index) => {
+				document.querySelector(`#input-pin-${index}`).value = digit;
+				const nextfield = document.querySelector(`#input-pin-${index + 1}`);
+				if (nextfield !== null) {
+					nextfield.focus();
+				}
+				 event.preventDefault();
+			})
+		};
+
+		return (
+			<Box className={className}>
+				<Inline space={labelSizeMap.get(size)} alignY="center">
+					{Array.from({ length: digits })
+						.fill(0)
+						.map((_, index) => (
+							<input id={`input-pin-${index}`} key={index} onPaste={handleOnPaste} className={styles.inputDigit[size]} maxLength={3} name="digit" placeholder='' />
+							// <TextInput key={index} onPaste={handleOnPaste} className={styles.inputDigit[size]} size={size} maxLength={1}  name="digit" placeholder='' />
+						))}
+				</Inline>
+			</Box>
+		);
+};
 
 export default PinInput;
