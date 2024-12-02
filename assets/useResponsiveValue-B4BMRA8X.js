d288a2b14d5e58d6810ad11528f2f8d61d35884d
amir-zahedi <amir-zahedi@users.noreply.github.com>
2024-12-02 10:04:38 +1000

    deploy: fffdca544f442771c23f352d26809122c45579d3

---
+++ assets/useResponsiveValue-B4BMRA8X.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index-sWIihdb-.js";import{i}from"./index-TqjM1jfD.js";import{g as m}from"./Box-00gOjviF.js";import{u}from"./useMedia-CJa1WIfW.js";function b(r,f){if(!i)return null;if(!Array.isArray(r))return r;const e=u(["mobile","tablet","desktop","largeDesktop"]).reduce((t,o,s)=>(o&&(t=s+1),t),1);return a.useMemo(()=>m(r,e),[r,e])}export{b as u};
