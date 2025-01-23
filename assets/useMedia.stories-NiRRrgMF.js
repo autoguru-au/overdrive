fab21cadff5ae69373d21a6cba178d8025909324
amir-zahedi <amir-zahedi@users.noreply.github.com>
2025-01-24 08:29:24 +1000

    deploy: 418ce1b4e98168af50c44a6acc11cf5e2f41e9fa

---
+++ assets/useMedia.stories-NiRRrgMF.js
@@ -0,0 +1,26 @@
+import{r as e}from"./index-sWIihdb-.js";import{S as c}from"./Stack-Dxrrs7IU.js";import{T as t}from"./Text-JrbyFrSo.js";import{a as r}from"./makeTheme-BvwTE3C0.js";import{u as k}from"./useMedia-hr6DGAzk.js";import"./_commonjsHelpers-C932wzq6.js";import"./index-DVF0g4jg.js";import"./Box-9pCRhkmn.js";import"./useTextStyles-BwBuJjU4.js";import"./index-BTJTCqq2.js";import"./ThemeProvider-Dunuu5Ct.js";import"./index-CYx1ALmS.js";const _={title:"Utility/Hooks/useMedia",parameters:{chromatic:{disable:!0}}},s=()=>{const l=()=>{const[i,p,m,u]=k(["mobile","tablet","desktop","largeDesktop"]);return e.createElement(c,null,e.createElement(t,null,"isMobile: ",e.createElement(t,{strong:!0},i?"true":"false")," ","- ",r.mobile),e.createElement(t,null,"isTable: ",e.createElement(t,{strong:!0},p?"true":"false")," -"," ",r.tablet),e.createElement(t,null,"isDesktop:"," ",e.createElement(t,{strong:!0},m?"true":"false")," -"," ",r.desktop),e.createElement(t,null,"isLargeDesktop:"," ",e.createElement(t,{strong:!0},u?"true":"false")," -"," ",r.largeDesktop))};return e.createElement(l,null)};var o,a,n;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
+  const Impl = () => {
+    const [isMobile, isTable, isDesktop, isLargeDesktop] = useMedia(['mobile', 'tablet', 'desktop', 'largeDesktop']);
+    return <Stack>
+                <Text>
+                    isMobile: <Text strong>{isMobile ? 'true' : 'false'}</Text>{' '}
+                    - {breakpoints.mobile}
+                </Text>
+                <Text>
+                    isTable: <Text strong>{isTable ? 'true' : 'false'}</Text> -{' '}
+                    {breakpoints.tablet}
+                </Text>
+                <Text>
+                    isDesktop:{' '}
+                    <Text strong>{isDesktop ? 'true' : 'false'}</Text> -{' '}
+                    {breakpoints.desktop}
+                </Text>
+                <Text>
+                    isLargeDesktop:{' '}
+                    <Text strong>{isLargeDesktop ? 'true' : 'false'}</Text> -{' '}
+                    {breakpoints.largeDesktop}
+                </Text>
+            </Stack>;
+  };
+  return <Impl />;
+}`,...(n=(a=s.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const h=["Standard"];export{s as Standard,h as __namedExportsOrder,_ as default};
---
+++ assets/useResponsiveValue-B26KB-TG.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index-sWIihdb-.js";import{i}from"./index-BTJTCqq2.js";import{g as m}from"./Box-9pCRhkmn.js";import{u}from"./useMedia-hr6DGAzk.js";function b(r,f){if(!i)return null;if(!Array.isArray(r))return r;const e=u(["mobile","tablet","desktop","largeDesktop"]).reduce((t,o,s)=>(o&&(t=s+1),t),1);return a.useMemo(()=>m(r,e),[r,e])}export{b as u};
