e1f0f85a88be5b02a60adeb4bd04e6610fcd6d16
skinread <skinread@users.noreply.github.com>
2025-01-24 11:27:38 +1000

    deploy: 0092bd6ef653e84619fd11ba15331dde3529a2a5

---
+++ assets/VisuallyHidden.stories-CN38UEzw.js
@@ -0,0 +1,6 @@
+import{r as e}from"./index-sWIihdb-.js";import{V as l}from"./VisuallyHidden-BU0jx8NN.js";import{T as s}from"./Text-BsqUUngv.js";import"./_commonjsHelpers-C932wzq6.js";import"./Box-j8EUPCOH.js";import"./dataAttrs-BPvLuXwN.js";import"./useTextStyles-Ap-dpgpw.js";const E={title:"Utility/VisuallyHidden",component:l,args:{as:"div"},argTypes:{as:{options:["div","span","a"],control:{type:"select"}}}},i=o=>e.createElement(e.Fragment,null,e.createElement(s,null,"Bellow text is invisible"),e.createElement(l,{...o},e.createElement(s,null,"I'm not visually present on the screens"))),t=i.bind({});var r,a,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`args => <>
+        <Text>Bellow text is invisible</Text>
+        <VisuallyHidden {...args}>
+            <Text>I&apos;m not visually present on the screens</Text>
+        </VisuallyHidden>
+    </>`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const T=["Example"];export{t as Example,T as __namedExportsOrder,E as default};
