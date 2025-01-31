a638600fe99c266d80032cdebe0592b8d06a6133
Sean Kinread <dev@kinread.com>
2025-01-31 16:15:37 +1100

    feat: structure props

--- lib/components/DateTimePicker/DateTimePicker.stories.tsx
+++ lib/components/DateTimePicker/DateTimePicker.stories.tsx
@@ -3,15 +3,53 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { DateTimePicker } from './DateTimePicker';
 
+// demo times
+const times = [
+	{
+		label: 'Early morning',
+		description: '6 to 8 AM',
+		name: '6-8',
+	},
+	{
+		label: 'Morning',
+		description: '8 to 10 AM',
+		name: '8-10',
+	},
+	{
+		label: 'Mid-morning',
+		description: '10 AM to 12 PM',
+		name: '10-12',
+	},
+	{
+		label: 'Early afteroon',
+		description: '12 to 2 PM',
+		name: '12-2',
+	},
+	{
+		label: 'Day before',
+		description: 'Afternoon of day prior to service',
+		name: 'aft-before',
+	},
+];
+
 const meta: Meta<typeof DateTimePicker> = {
 	title: 'Forms & Input Fields/Date & Time Picker',
 	component: DateTimePicker,
-	args: {},
+	args: {
+		timeOptionItems: times,
+		allowPastDate: false,
+		calendar: {
+			firstDayOfWeek: 'mon',
+		},
+		lang: {
+			dateLabel: 'Date label',
+			timeLabel: 'Time label',
+		},
+	},
 	tags: ['beta'],
 };
 
 export default meta;
 type Story = StoryObj<typeof DateTimePicker>;
 
-/** Uncontrolled with custom icons */
 export const DropOffSelection: Story = {};
--- lib/components/DateTimePicker/DateTimePicker.tsx
+++ lib/components/DateTimePicker/DateTimePicker.tsx
@@ -1,47 +1,71 @@
-import { GregorianCalendar } from '@internationalized/date';
+import {
+	getLocalTimeZone,
+	// currently only supporting western-style Gregorian calendar
+	GregorianCalendar,
+	today,
+} from '@internationalized/date';
 import React from 'react';
-import { useButton, useCalendar, useLocale } from 'react-aria';
+import {
+	useButton,
+	useCalendar,
+	useDateFormatter,
+	useLocale,
+	type AriaCalendarProps,
+	type DateValue,
+} from 'react-aria';
 import { useCalendarState } from 'react-stately';
 
-import { OptionGrid } from '../OptionGrid/OptionGrid';
+import { odStyle } from '../../styles/sprinkles.css';
+import { OptionGrid, OptionItem } from '../OptionGrid/OptionGrid';
 
 import { CalendarGrid } from './CalendarGrid';
 
-// demo times
-const times = [
-	{
-		label: 'Early morning',
-		description: '6 to 8 AM',
-		name: '6-8',
-	},
-	{
-		label: 'Morning',
-		description: '8 to 10 AM',
-		name: '8-10',
-	},
-	{
-		label: 'Mid-morning',
-		description: '10 AM to 12 PM',
-		name: '10-12',
-	},
-	{
-		label: 'Early afteroon',
-		description: '12 to 2 PM',
-		name: '12-2',
-	},
-	{
-		label: 'Day before',
-		description: 'Afternoon of day prior to service',
-		name: 'aft-before',
-	},
-];
+const defaultEnglish = {
+	dateLabel: 'Date',
+	timeLabel: 'Time',
+} as const;
+
+type LangContent = keyof typeof defaultEnglish;
+
+export interface DateTimePickerProps<D extends DateValue> {
+	/**
+	 * The title for the date time picker
+	 */
+	title?: string;
+	/**
+	 * The label for the time option items
+	 */
+	optionsLabel?: string;
+	/**
+	 * The items to select from for time options. Currently time options are not tied to the day selection.
+	 */
+	timeOptionItems: OptionItem[];
+	/**
+	 * Allow date in the past
+	 */
+	allowPastDate?: boolean;
+	// firstDayOfWeek?: AriaCalendarProps<D>['firstDayOfWeek'];
+	/**
+	 * Calendar props passed through to the react-aria hook
+	 * ([docs](https://react-spectrum.adobe.com/react-aria/useCalendar.html))
+	 *
+	 * *Defaults*
+	 * - `defaultValue`: Today's date
+	 * - `firstDayOfWeek`: Monday
+	 */
+	calendar?: AriaCalendarProps<D>;
+	/**
+	 * Language content override
+	 */
+	lang?: Partial<Record<LangContent, string>>;
+}
 
 function createCalendar(identifier) {
 	if (identifier === 'gregory') {
 		return new GregorianCalendar();
 	}
 
-	throw new Error(`Unsupported calendar ${identifier}`);
+	throw new Error(`Unsupported calendar configured ${identifier}`);
 }
 
 function CalendarButton(props) {
@@ -54,43 +78,95 @@ function CalendarButton(props) {
 	);
 }
 
-export const DateTimePicker = (props) => {
-	// const [selectedDate, setSelectedDate] = useState(new Date('2025-01-30'));
+const dateTextPunctuationEN = (text: string) =>
+	text
+		.split(' ')
+		.map((word, idx) => (idx === 0 ? `${word},` : word))
+		.join(' ');
+
+/**
+ * DateTimePicker component for selecting a date and time. The primary use case is for selecting a date and time for
+ * the vehicle to be left at the place of service, rather than the scheduling the time of the service itself.
+ *
+ * It is recommended to use the the DateTimePicker uncontrolled.
+ *
+ * This component implements the react-aria `useCalendar` hook and supports controlled state as well
+ * ([docs](https://react-spectrum.adobe.com/react-aria/useCalendar.html))
+ */
+export const DateTimePicker = <D extends DateValue>({
+	allowPastDate = false,
+	calendar,
+	lang,
+	optionsLabel,
+	timeOptionItems,
+}: DateTimePickerProps<D>) => {
+	const calendarComponentProps: AriaCalendarProps<D> = {
+		defaultValue: today(getLocalTimeZone()) as D,
+		firstDayOfWeek: 'sun',
+		minValue: allowPastDate ? undefined : today(getLocalTimeZone()),
+		...calendar,
+	};
 	const { locale } = useLocale();
 	const state = useCalendarState({
-		...props,
+		...calendarComponentProps,
 		locale,
 		createCalendar,
 	});
 
-	const { calendarProps, prevButtonProps, nextButtonProps, title } =
-		useCalendar(props, state);
+	const {
+		calendarProps,
+		prevButtonProps,
+		nextButtonProps,
+		title: calendarTitle,
+	} = useCalendar(calendarComponentProps, state);
 
-	// const calendar = createCalendar({
-	// 	weekStartsOn: 'sunday',
-	// });
+	const formatter = useDateFormatter({ dateStyle: 'full' });
+	const dateText = state.value
+		? dateTextPunctuationEN(
+				formatter.format(state?.value?.toDate(getLocalTimeZone())),
+			)
+		: '';
 
 	return (
 		<div>
-			<h2>Book an Appointment</h2>
-			<div>
-				<div {...calendarProps}>
-					<h3>{title}</h3>
-					<CalendarButton {...prevButtonProps}>Prev</CalendarButton>
-					<CalendarButton {...nextButtonProps}>Next</CalendarButton>
+			<div
+				className={odStyle({
+					display: 'flex',
+					gap: '5',
+				})}
+			>
+				<div className={odStyle({ flexShrink: 0 })}>
+					<h3>{lang?.dateLabel ?? defaultEnglish.dateLabel}</h3>
+					<div
+						{...calendarProps}
+						className={odStyle({ display: 'flex' })}
+					>
+						<CalendarButton {...prevButtonProps}>
+							Prev
+						</CalendarButton>
+						<h3>{calendarTitle}</h3>
+						<CalendarButton {...nextButtonProps}>
+							Next
+						</CalendarButton>
+					</div>
+					<CalendarGrid
+						state={state}
+						firstDayOfWeek={calendarComponentProps.firstDayOfWeek}
+					/>
+					{state.value && <h2>{dateText}</h2>}
 				</div>
-				<CalendarGrid state={state} firstDayOfWeek="mon" />
-			</div>
 
-			<div>
-				{/* <h2>{selectedDate.toLocaleDateString()}</h2> */}
-				<OptionGrid
-					columns="2"
-					label="Select a drop off time"
-					items={times}
-					indicator="none"
-					selectionMode="single"
-				/>
+				<div className={odStyle({ flexGrow: 1 })}>
+					<h3>{lang?.timeLabel ?? defaultEnglish.timeLabel}</h3>
+					<h4>{optionsLabel}</h4>
+					<OptionGrid
+						columns="2"
+						label="Select a drop off time"
+						items={timeOptionItems}
+						indicator="none"
+						selectionMode="single"
+					/>
+				</div>
 			</div>
 		</div>
 	);
