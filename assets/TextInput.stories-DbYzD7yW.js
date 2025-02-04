b3b08de3bd4f87ada2493d75544219e1eb4aff3c
skinread <skinread@users.noreply.github.com>
2025-02-05 09:41:30 +1000

    deploy: eab344819217841d297baf27782612bc0bf9c151

---
+++ assets/TextInput.stories-DbYzD7yW.js
@@ -0,0 +1,89 @@
+import{a as T}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{b as Ae}from"./argTypes-C5cQ_jgM.js";import{T as We}from"./TextInput-Cst-xwd4.js";import{a as b,b as V,I as ve,c as Ce}from"./CurrencyUsdIcon-MnlNA9b3.js";import{I as Pe}from"./CarIcon-BPYgb7mD.js";import{I as Oe}from"./AlertCircleIcon-BOrB_4ur.js";import{I as Be}from"./PlusIcon-BwhzlTq3.js";import{I as Ne}from"./StarIcon-DwMJajzk.js";import{I as Ee}from"./CheckIcon-qb3DRtZd.js";import"./v4-CQkTLCs1.js";import"./tokens-BrgPbGY2.js";import"./makeTheme-BvwTE3C0.js";import"./index-Cr_cdoBq.js";import"./_commonjsHelpers-C932wzq6.js";import"./withEnhancedInput-D1Vb5tBY.js";import"./index-CYx1ALmS.js";import"./Box-B8HP0AOd.js";import"./index-liKTkxwF.js";import"./Text-XoxmVRhK.js";import"./dataAttrs-BPvLuXwN.js";import"./useTextStyles-Db9gttPw.js";import"./withEnhancedInput.css-BuEZEUIc.js";import"./Icon-B6rvmQwO.js";import"./ProgressSpinner-FybsaYnS.js";const S="Jane Doe",e="What is your first name?",L={CalendarIcon:b,AccountEditIcon:V,AlertCircleIcon:Oe,CarMultipleIcon:ve,CurrencyUsdIcon:Ce,PlusIcon:Be,StarIcon:Ne,CheckIcon:Ee},ye={NONE:"NONE",TOP:"TOP",RIGHT:"RIGHT",LEFT:"LEFT",BOTTOM:"BOTTOM",ALL:"ALL"},sr={title:"Forms & Input Fields/Text Input",component:We,args:{disabled:!1,name:"text",placeholder:e,isValid:!1,isTouched:!1,isLoading:!1,isFocused:!1,reserveHintSpace:!1,hintText:"",notch:!0,prefixIcon:void 0,onChange:T("onChange"),onFocus:T("onFocus"),onBlur:T("onBlur")},argTypes:{attach:{defaultValue:"NONE",description:"Input attach",options:ye,control:{type:"select"}},prefixIcon:{defaultValue:null,description:"Input prefix Icon",options:L,control:{type:"select"}},suffixIcon:{defaultValue:null,description:"Input suffix Icon",options:L,control:{type:"select"}},backgroundColour:Ae.backgroundColour,maxLength:{defaultValue:null,description:"Set the max length of the input",control:{type:"number"}}}},a={},r={args:{value:S,placeholder:e}},o={args:{hintText:"Hint Text",placeholder:e}},s={args:{placeholder:e,notch:!1}},t={args:{value:S,placeholder:e,notch:!1}},n={args:{prefixIcon:b}},c={args:{suffixIcon:V}},l={args:{prefixIcon:b,suffixIcon:V}},i={args:{attach:"ALL"}},d={args:{borderMerged:"ALL"}},u={args:{value:S,placeholder:e,disabled:!0}},p={args:{value:S,placeholder:e,isTouched:!0,isValid:!0}},m={args:{value:"Bob the builder",placeholder:e,isTouched:!0,isValid:!1,hintText:"Cannot be Bob the builder"}},h={args:{isLoading:!0}},g={args:{size:"small"}},f={args:{...r.args,size:"small"}},I={args:{...r.args,prefixIcon:Pe,size:"small"}},x={args:{...r.args,isLoading:!0,size:"small"}};var A,W,v;a.parameters={...a.parameters,docs:{...(A=a.parameters)==null?void 0:A.docs,source:{originalSource:"{}",...(v=(W=a.parameters)==null?void 0:W.docs)==null?void 0:v.source}}};var C,P,O;r.parameters={...r.parameters,docs:{...(C=r.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    value: defaultValue,
+    placeholder: defaultPlaceholder
+  }
+}`,...(O=(P=r.parameters)==null?void 0:P.docs)==null?void 0:O.source}}};var B,N,E;o.parameters={...o.parameters,docs:{...(B=o.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    hintText: 'Hint Text',
+    placeholder: defaultPlaceholder
+  }
+}`,...(E=(N=o.parameters)==null?void 0:N.docs)==null?void 0:E.source}}};var y,z,$;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    placeholder: defaultPlaceholder,
+    notch: false
+  }
+}`,...($=(z=s.parameters)==null?void 0:z.docs)==null?void 0:$.source}}};var D,F,H;t.parameters={...t.parameters,docs:{...(D=t.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    value: defaultValue,
+    placeholder: defaultPlaceholder,
+    notch: false
+  }
+}`,...(H=(F=t.parameters)==null?void 0:F.docs)==null?void 0:H.source}}};var M,k,G;n.parameters={...n.parameters,docs:{...(M=n.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    prefixIcon: CalendarIcon
+  }
+}`,...(G=(k=n.parameters)==null?void 0:k.docs)==null?void 0:G.source}}};var R,_,J;c.parameters={...c.parameters,docs:{...(R=c.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    suffixIcon: AccountEditIcon
+  }
+}`,...(J=(_=c.parameters)==null?void 0:_.docs)==null?void 0:J.source}}};var U,j,q;l.parameters={...l.parameters,docs:{...(U=l.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    prefixIcon: CalendarIcon,
+    suffixIcon: AccountEditIcon
+  }
+}`,...(q=(j=l.parameters)==null?void 0:j.docs)==null?void 0:q.source}}};var w,K,Q;i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    attach: 'ALL'
+  }
+}`,...(Q=(K=i.parameters)==null?void 0:K.docs)==null?void 0:Q.source}}};var X,Y,Z;d.parameters={...d.parameters,docs:{...(X=d.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    borderMerged: 'ALL'
+  }
+}`,...(Z=(Y=d.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var ee,re,ae;u.parameters={...u.parameters,docs:{...(ee=u.parameters)==null?void 0:ee.docs,source:{originalSource:`{
+  args: {
+    value: defaultValue,
+    placeholder: defaultPlaceholder,
+    disabled: true
+  }
+}`,...(ae=(re=u.parameters)==null?void 0:re.docs)==null?void 0:ae.source}}};var oe,se,te;p.parameters={...p.parameters,docs:{...(oe=p.parameters)==null?void 0:oe.docs,source:{originalSource:`{
+  args: {
+    value: defaultValue,
+    placeholder: defaultPlaceholder,
+    isTouched: true,
+    isValid: true
+  }
+}`,...(te=(se=p.parameters)==null?void 0:se.docs)==null?void 0:te.source}}};var ne,ce,le;m.parameters={...m.parameters,docs:{...(ne=m.parameters)==null?void 0:ne.docs,source:{originalSource:`{
+  args: {
+    value: 'Bob the builder',
+    placeholder: defaultPlaceholder,
+    isTouched: true,
+    isValid: false,
+    hintText: 'Cannot be Bob the builder'
+  }
+}`,...(le=(ce=m.parameters)==null?void 0:ce.docs)==null?void 0:le.source}}};var ie,de,ue;h.parameters={...h.parameters,docs:{...(ie=h.parameters)==null?void 0:ie.docs,source:{originalSource:`{
+  args: {
+    isLoading: true
+  }
+}`,...(ue=(de=h.parameters)==null?void 0:de.docs)==null?void 0:ue.source}}};var pe,me,he;g.parameters={...g.parameters,docs:{...(pe=g.parameters)==null?void 0:pe.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(he=(me=g.parameters)==null?void 0:me.docs)==null?void 0:he.source}}};var ge,fe,Ie;f.parameters={...f.parameters,docs:{...(ge=f.parameters)==null?void 0:ge.docs,source:{originalSource:`{
+  args: {
+    ...WithAValue.args,
+    size: 'small'
+  }
+}`,...(Ie=(fe=f.parameters)==null?void 0:fe.docs)==null?void 0:Ie.source}}};var xe,Se,Te;I.parameters={...I.parameters,docs:{...(xe=I.parameters)==null?void 0:xe.docs,source:{originalSource:`{
+  args: {
+    ...WithAValue.args,
+    prefixIcon: CarIcon,
+    size: 'small'
+  }
+}`,...(Te=(Se=I.parameters)==null?void 0:Se.docs)==null?void 0:Te.source}}};var be,Ve,Le;x.parameters={...x.parameters,docs:{...(be=x.parameters)==null?void 0:be.docs,source:{originalSource:`{
+  args: {
+    ...WithAValue.args,
+    isLoading: true,
+    size: 'small'
+  }
+}`,...(Le=(Ve=x.parameters)==null?void 0:Ve.docs)==null?void 0:Le.source}}};const tr=["Standard","WithAValue","WithHintText","NotchDisabled","NotchDisabledWithValue","WithPrefixIcon","WithSuffixIcon","WithBothIcons","AttachedAll","MergedAll","Disabled","Valid","Invalid","Loading","Small","WithValueSmall","WithIconSmall","LoadingSmall"];export{i as AttachedAll,u as Disabled,m as Invalid,h as Loading,x as LoadingSmall,d as MergedAll,s as NotchDisabled,t as NotchDisabledWithValue,g as Small,a as Standard,p as Valid,r as WithAValue,l as WithBothIcons,o as WithHintText,I as WithIconSmall,n as WithPrefixIcon,c as WithSuffixIcon,f as WithValueSmall,tr as __namedExportsOrder,sr as default};
