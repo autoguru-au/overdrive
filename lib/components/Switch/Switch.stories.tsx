252e040bb8c54b5055d7a49bdabf9a0d059f7755
Sean Kinread <dev@kinread.com>
2025-02-06 18:16:03 +1100

    refactor: rewrite stories in CSF3

--- lib/components/Switch/Switch.stories.tsx
+++ lib/components/Switch/Switch.stories.tsx
@@ -6,40 +6,34 @@ import { Switch } from '.';
 const meta = {
 	title: 'Primatives/Switch',
 	component: Switch,
+	args: {
+		isDisabled: false,
+		isSelected: false,
+		onChange: action('onChange'),
+		className: 'toggleButton-class',
+	},
 } satisfies Meta<typeof Switch>;
 
 export default meta;
 
 type Story = StoryObj<typeof Switch>;
 
-const defaultArgs = {
-	isDisabled: false,
-	isSelected: false,
-	onChange: action('onChange'),
-	className: 'toggleButton-class',
-};
-
-export const Untoggled: Story = {
-	args: defaultArgs,
-};
+export const Untoggled: Story = {};
 
 export const UntoggledDisabled: Story = {
 	args: {
-		...defaultArgs,
 		isDisabled: true,
 	},
 };
 
 export const Toggled: Story = {
 	args: {
-		...defaultArgs,
 		isSelected: true,
 	},
 };
 
 export const ToggledDisabled: Story = {
 	args: {
-		...defaultArgs,
 		isSelected: true,
 		isDisabled: true,
 	},
