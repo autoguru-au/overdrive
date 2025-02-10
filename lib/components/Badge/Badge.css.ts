17db2019faebade1f3e0681db4c14ad5f0226370
Sean Kinread <dev@kinread.com>
2025-02-11 09:56:18 +1100

    Improve Badge component (#1038)
    * refactor: replace styled variants with reciepes, consolidate styling, adjust`small` font weight
    
    * fix: remove classname checking, snap
    
    * chore: another attempt to fine tune CI workflow
    
    * feat: intentional colours variants and recipe, more text styles in sprinkles
    
    * feat: update foreground colour of intent `success` to improve contrast
    
    * fix: Badge uses a Box to ensure border radius is dynamically themed

--- lib/components/Badge/Badge.css.ts
+++ lib/components/Badge/Badge.css.ts
@@ -1,89 +1,122 @@
-import { style, styleVariants } from '@vanilla-extract/css';
+import { recipe, type RecipeVariants } from '@vanilla-extract/recipes';
 
-import { themeContractVars as vars } from '../../themes/theme.css';
+import { styledIntentionalElement } from '../../styles/intentional.css';
+import { odStyle } from '../../styles/sprinkles.css';
 
-export const label = style({
-	textOverflow: 'ellipsis',
-	letterSpacing: '0.5px',
-	textTransform: 'uppercase',
+export const styledBadge = recipe({
+	base: [
+		odStyle({
+			lineHeight: 'match',
+			overflow: 'hidden',
+			padding: '1',
+			textOverflow: 'ellipsis',
+			textTransform: 'uppercase',
+			whiteSpace: 'nowrap',
+		}),
+		{
+			letterSpacing: '0.5px',
+		},
+	],
+	variants: {
+		colour: {
+			neutral: {},
+			red: {},
+			green: {},
+			blue: {},
+			yellow: {},
+		},
+		inverted: {
+			true: {},
+			false: {},
+		},
+		size: {
+			small: odStyle({
+				fontSize: 'xxs',
+				fontWeight: 'bold',
+			}),
+			standard: odStyle({
+				fontSize: 'xs',
+				fontWeight: 'semibold',
+			}),
+			large: odStyle({
+				fontSize: 'md',
+				fontWeight: 'semibold',
+				padding: '4',
+			}),
+		},
+	},
+	compoundVariants: [
+		{
+			variants: { colour: 'neutral', inverted: false },
+			style: styledIntentionalElement({
+				intention: 'neutral',
+				inverted: false,
+			}),
+		},
+		{
+			variants: { colour: 'neutral', inverted: true },
+			style: styledIntentionalElement({
+				intention: 'neutral',
+				inverted: true,
+			}),
+		},
+		{
+			variants: { colour: 'red', inverted: false },
+			style: styledIntentionalElement({
+				intention: 'danger',
+				inverted: false,
+			}),
+		},
+		{
+			variants: { colour: 'red', inverted: true },
+			style: styledIntentionalElement({
+				intention: 'danger',
+				inverted: true,
+			}),
+		},
+		{
+			variants: { colour: 'green', inverted: false },
+			style: styledIntentionalElement({
+				intention: 'success',
+				inverted: false,
+			}),
+		},
+		{
+			variants: { colour: 'green', inverted: true },
+			style: styledIntentionalElement({
+				intention: 'success',
+				inverted: true,
+			}),
+		},
+		{
+			variants: { colour: 'blue', inverted: false },
+			style: styledIntentionalElement({
+				intention: 'information',
+				inverted: false,
+			}),
+		},
+		{
+			variants: { colour: 'blue', inverted: true },
+			style: styledIntentionalElement({
+				intention: 'information',
+				inverted: true,
+			}),
+		},
+		{
+			variants: { colour: 'yellow', inverted: false },
+			style: styledIntentionalElement({
+				intention: 'warning',
+				inverted: false,
+			}),
+		},
+		{
+			variants: { colour: 'yellow', inverted: true },
+			style: styledIntentionalElement({
+				intention: 'warning',
+				inverted: true,
+			}),
+		},
+	],
 });
 
-export const labelSize = styleVariants({
-	small: {
-		fontSize: vars.typography.size['1'].fontSize,
-		lineHeight: vars.typography.size['1'].fontSize,
-	},
-	standard: {
-		fontSize: vars.typography.size['2'].fontSize,
-		lineHeight: vars.typography.size['2'].fontSize,
-	},
-	large: {
-		fontSize: vars.typography.size['4'].fontSize,
-		lineHeight: vars.typography.size['4'].fontSize,
-	},
-});
-
-// TODO: Derive the inverted colours from a token
-export const colours = {
-	default: styleVariants({
-		neutral: {
-			backgroundColor: vars.colours.intent.neutral.background.standard,
-		},
-		green: {
-			backgroundColor: vars.colours.intent.success.background.standard,
-		},
-		blue: {
-			backgroundColor:
-				vars.colours.intent.information.background.standard,
-		},
-		yellow: {
-			backgroundColor: vars.colours.intent.warning.background.standard,
-		},
-		red: {
-			backgroundColor: vars.colours.intent.danger.background.standard,
-		},
-	}),
-	inverted: {
-		neutral: styleVariants({
-			background: {
-				backgroundColor: vars.colours.intent.neutral.background.mild,
-			},
-			text: {
-				color: vars.colours.intent.neutral.background.standard,
-			},
-		}),
-		green: styleVariants({
-			background: {
-				backgroundColor: vars.colours.intent.success.background.mild,
-			},
-			text: {
-				color: vars.colours.intent.success.background.standard,
-			},
-		}),
-		blue: styleVariants({
-			background: {
-				backgroundColor:
-					vars.colours.intent.information.background.mild,
-			},
-			text: {
-				color: vars.colours.intent.information.background.standard,
-			},
-		}),
-		yellow: styleVariants({
-			background: {
-				backgroundColor: vars.colours.intent.warning.background.mild,
-			},
-			text: {
-				color: vars.colours.intent.warning.background.standard,
-			},
-		}),
-		red: styleVariants({
-			background: {
-				backgroundColor: vars.colours.intent.danger.background.mild,
-			},
-			text: {
-				color: vars.colours.intent.danger.background.standard,
-			},
-		}),
-	},
-};
+export type StyledBadgeProps = NonNullable<RecipeVariants<typeof styledBadge>>;
