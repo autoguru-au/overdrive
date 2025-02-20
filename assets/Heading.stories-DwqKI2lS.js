8406fd17bbf4db8fdb04baf043858d3dff50b962
skinread <skinread@users.noreply.github.com>
2025-02-21 09:02:42 +1000

    deploy: 3a80574ace1703009950d608fdec31f1ad9d994e

---
+++ assets/Heading.stories-DwqKI2lS.js
@@ -0,0 +1,36 @@
+import{R as e}from"./index-Cr_cdoBq.js";import{H as s}from"./Heading-DlTFW9hB.js";import"./_commonjsHelpers-C932wzq6.js";import"./dataAttrs-BPvLuXwN.js";import"./Box-ByZNOjEZ.js";import"./useTextStyles-c8XbNcHW.js";const x=[!1,!0],w=[!1,!0],O=["uppercase","capitalize",void 0],V=["normal","semiBold","bold"],y=["h1","h2","h3","h4","h5","h6"],W=["1","2","3","4","5","6","7","8","9"],k=["left","center","right"],f=["dark","light","neutral","primary","secondary","white","information","link","success","danger","warning","shine"],A={title:"Primatives/Heading",component:s,argTypes:{noWrap:{options:x,defaultValue:!1,control:{type:"boolean"}},breakWord:{options:w,defaultValue:!1,control:{type:"boolean"}},transform:{options:O,defaultValue:null,control:{type:"select"}},weight:{options:V,defaultValue:null,control:{type:"select"}},size:{options:W,defaultValue:void 0,control:{type:"select"}},align:{options:k,defaultValue:"left",control:{type:"select"}},colour:{options:f,defaultValue:void 0,control:{type:"select"}},is:{options:y,defaultValue:"h1",table:{type:{summary:"heading"},defaultValue:"h1"},description:"HTML heading tag to be used",control:{type:"select"}}}},n={args:{is:"h1",children:"I am a heading"},render:t=>e.createElement("div",{style:{maxWidth:"350px",width:"100%"}},e.createElement(s,{...t}))},r={args:{children:"I am a heading"},render:t=>e.createElement("div",{style:{maxWidth:"350px",width:"100%"}},y.map(a=>e.createElement(s,{key:a,...t,is:a})))},o={args:{children:"I am a heading"},render:t=>e.createElement("div",{style:{maxWidth:"350px",width:"100%"}},f.map((a,v)=>e.createElement("div",{key:v,style:{marginBottom:8}},e.createElement(s,{...t,colour:a}))))};var i,l,d;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    is: 'h1',
+    children: 'I am a heading'
+  },
+  render: args => <div style={{
+    maxWidth: '350px',
+    width: '100%'
+  }}>
+            <Heading {...args} />
+        </div>
+}`,...(d=(l=n.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};var c,p,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'I am a heading'
+  },
+  render: args => <div style={{
+    maxWidth: '350px',
+    width: '100%'
+  }}>
+            {headingTypeOptions.map(is => <Heading key={is} {...args} is={is} />)}
+        </div>
+}`,...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var u,h,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: 'I am a heading'
+  },
+  render: args => <div style={{
+    maxWidth: '350px',
+    width: '100%'
+  }}>
+            {colourOptions.map((colour, index) => <div key={index} style={{
+      marginBottom: 8
+    }}>
+                    <Heading {...args} colour={colour} />
+                </div>)}
+        </div>
+}`,...(g=(h=o.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const B=["Standard","AllTypes","AllColours"];export{o as AllColours,r as AllTypes,n as Standard,B as __namedExportsOrder,A as default};
---
+++ assets/palette.stories-Dw3dd_-0.js
@@ -0,0 +1,9 @@
+import{c as p}from"./Box-ByZNOjEZ.js";import{R as e}from"./index-Cr_cdoBq.js";import{o as d}from"./sprinkles.css-BmmMAnkI.js";import{b as g}from"./tokens-_1AlMUnv.js";import{S as s,l as E,C as u,h}from"./index-rn2K_ixw.js";import{H as n}from"./Heading-DlTFW9hB.js";import"./_commonjsHelpers-C932wzq6.js";import"./createRuntimeFn-62c9670f.esm-Bn1gNn0k.js";import"./makeTheme-BvwTE3C0.js";import"./dataAttrs-BPvLuXwN.js";import"./useTextStyles-c8XbNcHW.js";const P=({colour:t,hex:o,hue:a})=>e.createElement(s,{horizontal:!0,alignItems:"center",style:{gap:"10px",position:"relative"}},e.createElement(u,{background:t,size:"lg"},e.createElement("div",{className:h},o)),a&&t.replace(a,"")),S=({hue:t,shades:o})=>e.createElement(s,{space:"sm"},Object.entries(o).reverse().map(([a,i])=>e.createElement(P,{colour:`${t}${a}`,hex:i,hue:t,key:a}))),b=()=>e.createElement(s,{horizontal:!0,space:"md"},["green","blue","yellow","red","gray","black"].map(t=>e.createElement("div",{key:t},e.createElement(n,{is:"h3",className:p([E,d({marginBottom:"5"})])},t),e.createElement(S,{hue:t,shades:g[t]})))),R={title:"Foundation/Palette",tags:["!autodocs"]},r={render:()=>e.createElement(s,{space:"md"},e.createElement(n,{is:"h1"},"Palette"),e.createElement(n,{is:"h2"},"Colours"),e.createElement(b,null))};var l,m,c;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => {
+    return <Stack space="md">
+                <Heading is="h1">Palette</Heading>
+                <Heading is="h2">Colours</Heading>
+                <Palettes />
+            </Stack>;
+  }
+}`,...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const _=["Palette"];export{r as Palette,_ as __namedExportsOrder,R as default};
