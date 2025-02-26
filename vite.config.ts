c3c6626b54d90ee4f1b2d7fec7d7b1e66cfd3e53
Sean Kinread <dev@kinread.com>
2025-02-26 15:22:39 +1100

    chore: configures bundle analasis via rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,66 @@
+import path from 'node:path';
+
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import { configDefaults, defineConfig } from 'vitest/config';
+import { visualizer } from 'rollup-plugin-visualizer';
+import {
+	configDefaults,
+	defineConfig,
+	type ViteUserConfig,
+} from 'vitest/config';
+
+const analyseOutput = 'dist-analyse';
+const isAnalyse = process.env.ANALYSE === 'true';
+
+const analyseConfig: ViteUserConfig = {
+	build: {
+		lib: {
+			entry: path.resolve(import.meta.dirname, 'lib/index.ts'),
+			name: 'overdrive',
+			formats: ['es'],
+		},
+		outDir: analyseOutput,
+		rollupOptions: {
+			external: [
+				'react',
+				'react-dom',
+				'@autoguru/icons',
+				'@autoguru/utilities',
+				'@internationalized/date',
+				'@popperjs/core',
+				'@vanilla-extract/dynamic',
+				'clsx',
+				'colord',
+				'csstype',
+				'react-aria',
+				'react-aria-components',
+				'react-focus-lock',
+				'react-intersection-observer',
+				'react-keyed-flatten-children',
+				'react-swipeable',
+			],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
+};
 
 export default defineConfig({
-	plugins: [vanillaExtractPlugin()],
+	plugins: [
+		vanillaExtractPlugin(),
+		isAnalyse
+			? visualizer({
+					filename: `${analyseOutput}/stats.html`,
+					open: true,
+					gzipSize: true,
+					title: 'Overdrive dependencies',
+				})
+			: undefined,
+	],
+	// build: isAnalyse ? analyseConfig.build : undefined,
 	test: {
 		environment: 'jsdom',
 		globals: true,
