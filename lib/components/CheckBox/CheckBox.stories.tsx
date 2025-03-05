f5d8ceccd5a8c14206d9883f85054eab468fa387
Sean Kinread <dev@kinread.com>
2025-03-05 12:48:37 +1100

    feat: allows checkbox stories to be toggled

--- lib/components/CheckBox/CheckBox.stories.tsx
+++ lib/components/CheckBox/CheckBox.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
 import React from 'react';
 
 import { Badge } from '../Badge';
@@ -8,19 +9,6 @@ import { Text } from '../Text';
 
 import { CheckBox } from '.';
 
-const meta = {
-	title: 'Forms & Input Fields/CheckBox',
-	component: CheckBox,
-	decorators: [
-		(story) => (
-			<div style={{ maxWidth: '500px', width: '100%' }}>{story()}</div>
-		),
-	],
-} satisfies Meta<typeof CheckBox>;
-
-export default meta;
-type Story = StoryObj<typeof CheckBox>;
-
 const listData: Array<{ label: string; value: string }> = [
 	{ label: 'Avocado', value: 'avocado' },
 	{ label: 'Blueberries', value: 'blueberries' },
@@ -29,25 +17,88 @@ const listData: Array<{ label: string; value: string }> = [
 	{ label: 'Strawberries', value: 'strawberries' },
 ];
 
-export const List: Story = {
-	render: ({ disabled, ...args }) => (
-		<>
-			{listData.map((item) => (
-				<CheckBox
-					key={item.value}
-					disabled={disabled}
-					value={item.value}
-					name={`want-${item.value}`}
-					checked={args[item.value]}
-				>
-					{item.label}
-				</CheckBox>
-			))}
-		</>
-	),
+const meta: Meta<typeof CheckBox> = {
+	title: 'Forms & Input Fields/CheckBox',
+	component: CheckBox,
+	decorators: [
+		(Story) => (
+			<div style={{ maxWidth: '500px', width: '100%' }}>
+				<Story />
+			</div>
+		),
+	],
+	args: {
+		children: 'Check me!',
+		name: 'check-name',
+		onChange: fn(),
+		onClick: fn(),
+		value: '1',
+	},
+	render: ({ ...args }) => {
+		const [checked, setChecked] = React.useState(false);
+		return (
+			<CheckBox
+				{...args}
+				checked={checked}
+				onChange={(checked) => {
+					setChecked(checked);
+					args.onChange?.(checked);
+				}}
+			/>
+		);
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof CheckBox>;
+
+export const Default: Story = {};
+
+export const Disabled: Story = {
+	args: {
+		checked: false,
+		disabled: true,
+	},
+};
+
+export const List = {
+	render: ({ disabled, ...args }) => {
+		const [selected, setSelected] = React.useState(() => ({
+			avocado: args.avocado || false,
+			blueberries: args.blueberries || false,
+			cherries: args.cherries || false,
+			coconut: args.coconut || false,
+			strawberries: args.strawberries || false,
+		}));
+
+		const handleChange = (checked: boolean, value: string) => {
+			setSelected((prev) => ({
+				...prev,
+				[value]: checked,
+			}));
+		};
+
+		return (
+			<>
+				{listData.map((item) => (
+					<CheckBox
+						key={item.value}
+						disabled={disabled}
+						value={item.value}
+						name={`want-${item.value}`}
+						checked={selected[item.value]}
+						onChange={(checked) =>
+							handleChange(checked, item.value)
+						}
+					>
+						{item.label}
+					</CheckBox>
+				))}
+			</>
+		);
+	},
 	args: {
 		disabled: false,
-		// @ts-expect-error example values
 		avocado: true,
 		blueberries: true,
 		cherries: false,
@@ -56,21 +107,10 @@ export const List: Story = {
 	},
 };
 
-export const Disabled: Story = {
-	args: {
-		checked: false,
-		disabled: true,
-		name: 'check-name',
-		children: 'check me!',
-		value: '1',
-	},
-};
-
 export const MultipleLines: Story = {
 	args: {
 		checked: false,
 		disabled: false,
-		name: 'check-name',
 		children:
 			'There is a very good reason why this thing is a multi-line, sometimes we need to show people a lot of things. And thus this exists.',
 		value: '1',
@@ -94,18 +134,15 @@ export const WithComponent: Story = {
 	args: {
 		checked: false,
 		disabled: false,
-		name: 'check-name',
 		children: <Item label="Avocados" rating="4.3" />,
 		value: '1',
 	},
-	argTypes: { children: { control: { disable: true } } },
 };
 
 export const WithMultiLineComponent: Story = {
 	args: {
 		checked: false,
 		disabled: false,
-		name: 'check-name',
 		children: (
 			<div
 				style={{
@@ -132,5 +169,4 @@ export const WithMultiLineComponent: Story = {
 		),
 		value: '1',
 	},
-	argTypes: { children: { control: { disable: true } } },
 };
