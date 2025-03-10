b73d82fbb5e5a264df34a14270348dda71df49be
Sean Kinread <dev@kinread.com>
2025-03-10 11:38:43 +1100

    feat: add `interactionStyle` util

--- lib/utils/css.ts
+++ lib/utils/css.ts
@@ -1,3 +1,5 @@
+import type { StyleRule } from '@vanilla-extract/css';
+
 const cssVarRegExp = /var\(([^)]+)\)/;
 
 export const cssVarUnwrap = (value: string) => {
@@ -17,4 +19,59 @@ export const getThemeTokenValue = (
 	return (
 		getComputedStyle(themedElement).getPropertyValue(cssVar)?.trim() || ''
 	);
+};
+
+export type SelectorStates =
+	| 'base'
+	| 'active'
+	| 'checked'
+	| 'selected'
+	| 'disabled'
+	| 'focus'
+	| 'focusVisible'
+	| 'hover';
+export type InteractionStyleProps = Partial<Record<SelectorStates, StyleRule>>;
+type SelectorStateKeys = Exclude<SelectorStates, 'base' | 'checked'>;
+
+const notDisabled = ':not(:disabled,[data-disabled])';
+
+/**
+ * Creates style variants for different interaction states that can be spread
+ * within vanilla-extract `style` object.
+ *
+ * @example
+ * const buttonStyle = style({
+ *   backgroundColor: tokens.colours.background.primary,
+ *   color: tokens.colours.text.primary,
+ *   ...interactionStyle({
+ *     initial: { background: 'white', color: 'black' },
+ *     hover: { background: 'lightgray' },
+ *     disabled: { opacity: 0.5 }
+ *   })
+ * });
+ */
+export const interactionStyle = (
+	selectors: InteractionStyleProps,
+): StyleRule => {
+	const { base = {}, ...interactionStyles } = selectors;
+
+	const styles = { ...base, selectors: {} };
+	const rules: Record<SelectorStateKeys, string> = {
+		hover: `&:hover${notDisabled}, &[data-hover]${notDisabled}`,
+		active: '&:active',
+		selected: '&:checked, &[data-checked], &[data-selected]',
+		focus: '&:focus, &[data-focus], &[data-focused]',
+		focusVisible: '&:focus-visible, &[data-focus-visible]',
+		disabled: '&:disabled, &[data-disabled]',
+	};
+
+	// Add selectors for each interaction state that has styles
+	Object.entries(interactionStyles).forEach(([state, stateStyles]) => {
+		if (state in rules && stateStyles) {
+			if (state === 'checked') state = 'selected';
+			styles.selectors[rules[state]] = stateStyles;
+		}
+	});
+
+	return styles;
 };
