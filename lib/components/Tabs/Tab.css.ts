159e36312209023f827ad288f72868dc643ff29b
Sean Kinread <dev@kinread.com>
2025-03-18 20:10:02 +1100

    Tabs pill variant (#1079)
    * feat: refactors tab style into recipes
    
    * feat: adds `appearance` prop to Tabs + context
    
    * feat: Tabs styling for `pill` with indicators
    
    * feat: `pill` Tabs spacing
    
    * test: snap

--- lib/components/Tabs/Tab.css.ts
+++ lib/components/Tabs/Tab.css.ts
@@ -1,4 +1,5 @@
-import { style, styleVariants } from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 import { themeContractVars as vars } from '../../themes/theme.css';
 
@@ -7,28 +8,74 @@ import { focusOutline } from './../../styles/focusOutline.css';
 const lineBottomHeight = '1px';
 const size = '20px';
 
-export const root = {
-	default: style({
-		padding: `calc(${vars.space['3']} + ${lineBottomHeight}) ${vars.space['4']}`,
-		transition: `color 0.2s ${vars.animation.easing.decelerate} 0s, background-color 0.2s ${vars.animation.easing.decelerate} 0s`,
-		borderBottom: `calc(${lineBottomHeight} + ${lineBottomHeight}) solid transparent`,
+export const styledTab = recipe({
+	base: {
 		flex: 'auto',
-		':last-of-type': {
-			marginRight: 0,
-		},
-		':hover': {
-			color: vars.colours.intent.neutral.background.strong,
-		},
+		transition: `color 0.2s ${vars.animation.easing.decelerate} 0s, background-color 0.2s ${vars.animation.easing.decelerate} 0s`,
 		':focus-visible': {
 			...focusOutline,
-			outlineOffset: '-2px',
 		},
-	}),
-	active: style({
-		color: vars.colours.intent.neutral.background.strong,
-		borderBottomColor: vars.colours.intent.neutral.background.strong,
-	}),
-};
+		selectors: {
+			'&:not([aria-selected=true]):hover': {
+				color: vars.colours.foreground.body,
+			},
+		},
+	},
+	variants: {
+		appearance: {
+			underlined: {
+				borderBottom: `calc(${lineBottomHeight} + ${lineBottomHeight}) solid transparent`,
+				padding: `calc(${vars.space['3']} + ${lineBottomHeight}) ${vars.space['4']}`,
+				':focus-visible': {
+					outlineOffset: '-1px',
+				},
+			},
+			pill: {
+				borderRadius: vars.border.radius.pill,
+				color: vars.colours.foreground.body,
+				fontWeight: vars.typography.fontWeight.normal,
+				padding: `${vars.space['2']} ${vars.space['4']}`,
+				selectors: {
+					'&+&': {
+						marginLeft: vars.space['3'],
+					},
+					'&:not([aria-selected=true]):hover': {
+						backgroundColor: vars.colours.gamut.gray200,
+					},
+				},
+			},
+		},
+		active: {
+			true: {},
+		},
+	},
+	compoundVariants: [
+		{
+			variants: {
+				appearance: 'underlined',
+				active: true,
+			},
+			style: {
+				color: vars.colours.intent.neutral.background.strong,
+				borderBottomColor:
+					vars.colours.intent.neutral.background.strong,
+			},
+		},
+		{
+			variants: {
+				appearance: 'pill',
+				active: true,
+			},
+			style: {
+				backgroundColor: vars.colours.foreground.body,
+				color: vars.colours.background.body,
+			},
+		},
+	],
+	defaultVariants: {
+		appearance: 'underlined',
+	},
+});
 
 export const item = style({
 	display: 'inline-flex',
@@ -36,15 +83,57 @@ export const item = style({
 	verticalAlign: 'middle',
 });
 
-export const indication = styleVariants({
-	default: {
-		backgroundColor: vars.colours.background.light,
-		minWidth: size,
-		height: size,
-		lineHeight: size,
-		transition: `color 0.2s ${vars.animation.easing.decelerate} 0s, backgroundColor 0.2s ${vars.animation.easing.decelerate} 0s`,
+export const indication = recipe({
+	base: [
+		{
+			backgroundColor: vars.colours.background.light,
+			borderRadius: vars.border.radius.pill,
+			display: 'inline-block',
+			minWidth: size,
+			height: size,
+			padding: `0 ${vars.space['1']}`,
+			lineHeight: size,
+			transition: `color 0.2s ${vars.animation.easing.decelerate} 0s, backgroundColor 0.2s ${vars.animation.easing.decelerate} 0s`,
+		},
+	],
+	variants: {
+		appearance: {
+			underlined: {},
+			pill: {
+				selectors: {
+					[`${styledTab.classNames.base}:not([aria-selected=true]):hover &`]:
+						{
+							backgroundColor: vars.colours.background.neutral,
+						},
+				},
+			},
+		},
+		active: {
+			true: {},
+		},
 	},
-	active: {
-		backgroundColor: vars.colours.intent.neutral.background.strong,
+	compoundVariants: [
+		{
+			variants: {
+				appearance: 'underlined',
+				active: true,
+			},
+			style: {
+				backgroundColor: vars.colours.intent.neutral.background.strong,
+			},
+		},
+		{
+			variants: {
+				appearance: 'pill',
+				active: true,
+			},
+			style: {
+				backgroundColor: vars.colours.background.light,
+				color: vars.colours.foreground.body,
+			},
+		},
+	],
+	defaultVariants: {
+		appearance: 'underlined',
 	},
 });
--- lib/components/Tabs/TabList.tsx
+++ lib/components/Tabs/TabList.tsx
@@ -1,13 +1,13 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@autoguru/icons';
 import { invariant } from '@autoguru/utilities';
-import type { FunctionComponent } from 'react';
-import * as React from 'react';
-import {
+import React, {
 	Children,
+	type FunctionComponent,
 	createContext,
 	isValidElement,
-	ReactNode,
+	type ReactNode,
 	useCallback,
+	useContext,
 	useEffect,
 	useRef,
 	useState,
@@ -21,6 +21,7 @@ import { Icon } from '../Icon';
 import { useTextStyles } from '../Text';
 
 import * as styles from './TabList.css';
+import { TabsContext } from './Tabs';
 
 export interface Props {
 	stretch?: boolean;
@@ -58,6 +59,13 @@ export const TabList: FunctionComponent<Props> = ({
 		);
 	});
 
+	const tabsContext = useContext(TabsContext);
+	invariant(
+		tabsContext !== null,
+		'This tablist isnt nested beneath <Tabs />',
+	);
+	const { appearance } = tabsContext;
+
 	const [displayScroll, setDisplayScroll] = useState({
 		start: false,
 		end: false,
@@ -94,9 +102,9 @@ export const TabList: FunctionComponent<Props> = ({
 	};
 
 	const handleStartButton = () =>
-		void scrollToItem(-wrapperRef.current!.clientWidth!);
+		scrollToItem(-wrapperRef.current!.clientWidth!);
 	const handleEndButton = () =>
-		void scrollToItem(wrapperRef.current!.clientWidth!);
+		scrollToItem(wrapperRef.current!.clientWidth!);
 
 	useEffect(() => {
 		const win = ownerWindow(wrapperRef.current!);
@@ -113,19 +121,18 @@ export const TabList: FunctionComponent<Props> = ({
 
 	useEffect(() => {
 		updateScrollButtonState();
-	}, [children]);
+	}, [children, updateScrollButtonState]);
 
 	const shouldShowScrollButtons =
 		scrollable && (displayScroll.start || displayScroll.end);
 
 	return (
 		<Box
-			overflow="hidden"
 			alignItems="center"
-			className={[
-				styles.root.default,
-				shouldShowScrollButtons && styles.root.scroll,
-			]}
+			className={styles.styledTabList({
+				appearance,
+				scroll: scrollable,
+			})}
 		>
 			{shouldShowScrollButtons ? (
 				<Button
