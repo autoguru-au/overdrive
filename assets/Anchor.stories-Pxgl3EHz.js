a2803abc3082cdc6489ac135a6709473486ecb47
skinread <skinread@users.noreply.github.com>
2025-03-19 14:53:55 +1000

    deploy: fa47d39d3668999ef68522c8342247b427c9902e

---
+++ assets/Anchor.stories-Pxgl3EHz.js
@@ -0,0 +1,16 @@
+import{a as T}from"./argTypes-KiDJmtKj.js";import{c as V,u as v}from"./Box-BZBbKvYV.js";import{r as e}from"./index-UyvCXs0Z.js";import{u as B}from"./useTextStyles-ptSZJ0kX.js";import{I as N}from"./Inline-Tz3l7KBY.js";import{I as W}from"./Icon-DcPNnT7l.js";import{T as b}from"./Text-BLBByk4R.js";import{I as q}from"./PhoneIcon-Khtq4F7v.js";import{B as A}from"./Button-C8rarPIR.js";import"./CheckIcon-CoTLElTW.js";import"./StarIcon-CPn0OLOx.js";import"./PlusIcon-DUgVuZrH.js";import"./CurrencyUsdIcon-CUU6C2t6.js";import"./AlertCircleIcon-7gwXcgPK.js";import"./CarIcon-BVQRu3Hm.js";import"./MagnifyIcon-Bn7ALZbo.js";import"./_commonjsHelpers-gnU0ypJ3.js";import"./index-ByY9IofJ.js";import"./useNegativeMarginTop-DNM2Rbyz.js";import"./dataAttrs-C4KudU4k.js";import"./ProgressSpinner-Bcqw29R3.js";var k="um22v0";const o=({className:n="",is:r="a",disabled:x=!1,children:S,icon:i,..._})=>{const c=B({colour:"link"}),l={className:V(k,c,v({is:typeof r=="string"?r:void 0,display:"inline"}),n),disabled:x,..._},m=e.createElement(N,{space:"2"},i&&e.createElement(W,{icon:i,size:"small",className:c}),e.createElement(b,{fontWeight:"bold",size:"4",colour:"link"},S));return e.isValidElement(r)?e.cloneElement(r,l,m):e.createElement(r,l,m)};try{o.displayName="Anchor",o.__docgenInfo={description:"",displayName:"Anchor",props:{className:{defaultValue:{value:""},description:"",name:"className",required:!1,type:{name:"string"}},is:{defaultValue:null,description:"",name:"is",required:!1,type:{name:"ElementType | ReactElement<unknown, string | JSXElementConstructor<any>>"}},disabled:{defaultValue:{value:"false"},description:"",name:"disabled",required:!1,type:{name:"boolean"}},icon:{defaultValue:null,description:"",name:"icon",required:!1,type:{name:"IconType"}}}}}catch{}const ee={title:"Primatives/Anchor",component:o,argTypes:{icon:{defaultValue:null,description:"Input field Icon",...T},is:{control:{disable:!0}},children:{defaultValue:"07 5612 5347",control:{type:"text"}}}},t={args:{href:"tel:07 5612 5347",children:"07 5612 5347"}},a={args:{...t.args,icon:q}},s={args:{...a.args,is:A}};var p,d,u;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    href: 'tel:07 5612 5347',
+    children: '07 5612 5347'
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var f,g,y;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    ...Standard.args,
+    icon: PhoneIcon
+  }
+}`,...(y=(g=a.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var h,I,E;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    ...WithIcon.args,
+    is: Button
+  }
+}`,...(E=(I=s.parameters)==null?void 0:I.docs)==null?void 0:E.source}}};const re=["Standard","WithIcon","WithButton"];export{t as Standard,s as WithButton,a as WithIcon,re as __namedExportsOrder,ee as default};
---
+++ assets/TextLink.stories-_5lgf3I-.js
@@ -0,0 +1,33 @@
+import{r as a,R as t}from"./index-UyvCXs0Z.js";import{I as N}from"./ChevronRightIcon-DAtmVByr.js";import{I as S}from"./ArrowRightIcon-BImX12fV.js";import{d as A}from"./index-D_iG_Vvt.js";import{c as p,u as f,B as T}from"./Box-BZBbKvYV.js";import{T as B}from"./Text-BLBByk4R.js";import{I as O}from"./Icon-DcPNnT7l.js";import"./_commonjsHelpers-gnU0ypJ3.js";import"./dataAttrs-C4KudU4k.js";import"./useTextStyles-ptSZJ0kX.js";var H="_5ax1bk0",C="_5ax1bk1",D="_5ax1bk2";const s=a.forwardRef(({is:e,children:_,className:R,strong:z,fontWeight:w="semiBold",muted:m=!1,size:L,icon:o,...u},P)=>{A.invariant(!(e!==void 0&&u.href!==void 0),"You cannot have both href and as defined.");const d=a.createElement(B,{is:"span",colour:m?"muted":"link",size:L,fontWeight:w,strong:z,className:p(f({is:"span",pointerEvents:"none",position:"relative",paddingRight:o?"5":void 0}),{[D]:m})},_,o?a.createElement(O,{icon:o,size:"small",display:"inlineBlock",className:p(C,f({position:"absolute"}))}):null),c={rel:u.rel??"noopener noreferrer",...u,ref:P};return e===void 0?a.createElement(T,{is:"a",className:[R,H],...c},d):a.isValidElement(e)?a.cloneElement(e,c,d):a.createElement(e,c,d)});try{s.displayName="TextLink",s.__docgenInfo={description:"",displayName:"TextLink",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},is:{defaultValue:null,description:"",name:"is",required:!1,type:{name:"ElementType | ReactElement<unknown, string | JSXElementConstructor<any>>"}},muted:{defaultValue:{value:"false"},description:"",name:"muted",required:!1,type:{name:"boolean"}},size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:"enum",value:[{value:'"1"'},{value:'"2"'},{value:'"3"'},{value:'"4"'},{value:'"5"'},{value:'"6"'},{value:'"7"'},{value:'"8"'},{value:'"9"'}]}},id:{defaultValue:null,description:"",name:"id",required:!1,type:{name:"string"}},style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties"}},display:{defaultValue:null,description:"",name:"display",required:!1,type:{name:"enum",value:[{value:'"block"'},{value:'"inline"'},{value:'"inlineBlock"'}]}},strong:{defaultValue:null,description:"",name:"strong",required:!1,type:{name:"boolean"}},fontWeight:{defaultValue:{value:"semiBold"},description:"",name:"fontWeight",required:!1,type:{name:"enum",value:[{value:'"normal"'},{value:'"bold"'},{value:'"semiBold"'}]}},transform:{defaultValue:null,description:"",name:"transform",required:!1,type:{name:"enum",value:[{value:'"capitalize"'},{value:'"uppercase"'}]}},icon:{defaultValue:null,description:"",name:"icon",required:!1,type:{name:"IconType"}},testId:{defaultValue:null,description:"The test ID will be rendered as a data attribute on the element for e2e testing purposes",name:"testId",required:!1,type:{name:"string"}},noWrap:{defaultValue:null,description:"",name:"noWrap",required:!1,type:{name:"boolean"}},align:{defaultValue:null,description:"@deprecated Because when you go `useTextStyles` for alignment, you should be using `useBoxStyles`",name:"align",required:!1,type:{name:"enum",value:[{value:'"center"'},{value:'"left"'},{value:'"right"'}]}},breakWord:{defaultValue:null,description:"",name:"breakWord",required:!1,type:{name:"boolean"}}}}}catch{}const J=["1","2","3","4","5","6","7","8","9"],X=["left","center","right"],Y=["normal","semiBold","bold"],j=[!1,!0],F=["uppercase","capitalize",void 0],te={title:"Primatives/Text Link",component:s,decorators:[e=>t.createElement("div",{style:{width:"100%",maxWidth:300,display:"grid",gridTemplateColumns:"1fr"}},t.createElement(e,null))],argTypes:{icon:{defaultValue:null,description:"Input field Icon",options:["Arrow Right","Chevron Right"],mapping:{"Arrow Right":S,"Chevron Right":N}},noWrap:{options:j,defaultValue:!1,control:{type:"boolean"}},transform:{options:F,defaultValue:null,control:{type:"select"}},fontWeight:{options:Y,defaultValue:null,control:{type:"select"}},size:{options:J,defaultValue:void 0,control:{type:"select"}},align:{options:X,defaultValue:"left",control:{type:"select"}},is:{control:{disable:!0}}}},r={args:{children:"Hello",muted:!1,size:"4",align:"left",fontWeight:"semiBold"},render:e=>t.createElement(T,null,t.createElement(s,{...e}))},n={args:{...r.args},render:e=>t.createElement(B,{is:"p"},"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad,"," ",t.createElement(s,{...e})," autem consectetur consequuntur eius fugiat illo ipsum nobis numquam, officiis placeat quia, quidem reprehenderit rerum temporibus veniam vero.")},l={args:{...r.args,icon:S},render:r.render},i={args:{...l.args},render:n.render};var g,v,h;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello',
+    muted: false,
+    size: '4',
+    align: 'left',
+    fontWeight: 'semiBold'
+  },
+  render: args => <Box>
+            <TextLink {...args} />
+        </Box>
+}`,...(h=(v=r.parameters)==null?void 0:v.docs)==null?void 0:h.source}}};var y,b,x;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    ...Standard.args
+  },
+  render: args => <Text is="p">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad,{' '}
+            <TextLink {...args} /> autem consectetur consequuntur eius fugiat
+            illo ipsum nobis numquam, officiis placeat quia, quidem
+            reprehenderit rerum temporibus veniam vero.
+        </Text>
+}`,...(x=(b=n.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var I,q,V;l.parameters={...l.parameters,docs:{...(I=l.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    ...Standard.args,
+    icon: ArrowRightIcon
+  },
+  render: Standard.render
+}`,...(V=(q=l.parameters)==null?void 0:q.docs)==null?void 0:V.source}}};var E,W,k;i.parameters={...i.parameters,docs:{...(E=i.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    ...WithIcon.args
+  },
+  render: InsideParagraph.render
+}`,...(k=(W=i.parameters)==null?void 0:W.docs)==null?void 0:k.source}}};const ne=["Standard","InsideParagraph","WithIcon","WithIconInsideParagraph"];export{n as InsideParagraph,r as Standard,l as WithIcon,i as WithIconInsideParagraph,ne as __namedExportsOrder,te as default};
