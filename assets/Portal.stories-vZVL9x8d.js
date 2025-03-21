c31f4c7dc28cc3b5894c04f4ee7f7ccedd183110
skinread <skinread@users.noreply.github.com>
2025-03-21 13:43:36 +1000

    deploy: d496c49318d62245d2a9edd25f25dff3a26c94d7

---
+++ assets/Portal.stories-vZVL9x8d.js
@@ -0,0 +1,18 @@
+import{R as r}from"./index-UyvCXs0Z.js";import{_ as l}from"./Portal-v7eqpybR.js";import{T as p}from"./Text-BLBByk4R.js";import{S as d}from"./Stack-CInYNX1D.js";import{H as u}from"./Heading-5evDL_5L.js";import"./_commonjsHelpers-gnU0ypJ3.js";import"./index-ijGxv8TO.js";import"./index-rbvpFaDF.js";import"./index-BJSya_LC.js";import"./ThemeProvider-CiKbc3fE.js";import"./index-D_iG_Vvt.js";import"./makeTheme-BvwTE3C0.js";import"./dataAttrs-C4KudU4k.js";import"./useTextStyles-ptSZJ0kX.js";import"./Box-BZBbKvYV.js";import"./index-ByY9IofJ.js";const B={title:"Utility/Portal",component:l,parameters:{chromatic:{disable:!0}},argTypes:{children:{control:{disable:!0}},container:{control:{disable:!0}}}},o=document==null?void 0:document.getElementsByTagName("body")[0],t={args:{container:o,children:r.createElement(p,{colour:"primary"},"Im in a portal at the root.")}},e={args:{container:o,children:r.createElement(d,{space:"5"},r.createElement(u,{is:"h3",colour:"information"},"test child 1"),r.createElement(l,{container:o},r.createElement(p,{colour:"primary"},"test child 2")))}};var a,n,i;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    container: containerEl,
+    children: <Text colour="primary">Im in a portal at the root.</Text>
+  }
+}`,...(i=(n=t.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var c,s,m;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    container: containerEl,
+    children: <Stack space="5">
+                <Heading is="h3" colour="information">
+                    test child 1
+                </Heading>
+                <Portal container={containerEl}>
+                    <Text colour="primary">test child 2</Text>
+                </Portal>
+            </Stack>
+  }
+}`,...(m=(s=e.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const O=["Standard","Nested"];export{e as Nested,t as Standard,O as __namedExportsOrder,B as default};
