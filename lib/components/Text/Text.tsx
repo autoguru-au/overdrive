5ba53a3c7e2078489e0ca63d6f12dab929044d71
Sean Kinread <dev@kinread.com>
2025-05-14 14:43:59 +1000

    feat: migrate Text to useBox

--- lib/components/Text/Text.tsx
+++ lib/components/Text/Text.tsx
@@ -1,74 +1,57 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
-import { Sprinkles } from '../../styles/sprinkles.css';
-import type { WithTestId } from '../../types';
-import { dataAttrs } from '../../utils/dataAttrs';
-import type { UseBoxProps } from '../Box';
-import { Box } from '../Box';
+import { useBox, UseBoxProps, type BoxLikeProps } from '../Box';
 
-import { TextStyleProps, useTextStyles } from './useTextStyles';
+import {
+	type TextStylesProps,
+	type TextTags,
+	textStyles,
+} from './useTextStyles.css';
 
-type Display = Extract<
-	UseBoxProps['display'],
-	'inline' | 'inline-block' | 'block'
->;
-type ElementAttributes = React.ComponentPropsWithoutRef<'p'> &
-	Pick<React.ComponentProps<'label'>, 'htmlFor'>;
-
-export interface TextProps
-	extends Omit<ElementAttributes, 'color' | 'is'>,
-		TextStyleProps {
-	/** Set the text colour */
-	color?: Sprinkles['color'];
+interface CustomTextProps extends Omit<TextStylesProps, 'as'> {
 	/** Use bold font weight */
 	strong?: boolean;
-	/** Select CSS display property  */
-	display?: Display;
 }
 
-export const Text = React.forwardRef<HTMLElement, WithTestId<TextProps>>(
-	(
-		{
-			children,
-			className,
-			is = 'span',
-			as = is,
-			testId,
-			colour,
-			display,
-			fontWeight = 'normal',
-			transform,
-			breakWord,
-			noWrap,
-			size = '4',
-			strong = false,
-			...props
-		},
-		ref,
-	) => (
-		<Box
-			as={as}
-			ref={ref}
-			display={display}
-			className={[
-				useTextStyles({
-					as,
-					size,
-					colour: colour ?? (strong ? 'dark' : undefined),
-					fontWeight: strong ? 'bold' : fontWeight,
-					transform,
-					noWrap,
-					breakWord,
-				}),
-				className,
-			]}
-			{...props}
-			{...dataAttrs({ 'test-id': testId })}
-		>
-			{children}
-		</Box>
-	),
-);
+export type TextProps<E extends TextTags> = BoxLikeProps<E, CustomTextProps>;
+
+export const Text = <E extends TextTags>({
+	as = 'span' as E,
+	children,
+	className,
+	colour,
+	display,
+	fontWeight = 'normal',
+	transform,
+	breakWord,
+	noWrap,
+	size = '4',
+	strong = false,
+	...props
+}: TextProps<E>) => {
+	const styles = textStyles({
+		as: as as TextTags,
+		size,
+		colour: colour ?? (strong ? 'dark' : undefined),
+		fontWeight: strong ? 'bold' : fontWeight,
+		transform,
+		noWrap,
+		breakWord,
+	});
+
+	const { Component, componentProps, reactElement } = useBox<E>({
+		...(props as UseBoxProps<E>),
+		as,
+		className: [styles, className],
+		display,
+	});
+
+	if (reactElement) {
+		return cloneElement(reactElement, componentProps, children);
+	}
+
+	return <Component {...componentProps}>{children}</Component>;
+};
 
 Text.displayName = 'Text';
 
