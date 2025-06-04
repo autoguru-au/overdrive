1baec870a48ddde0e0c03293d961e1c55b93d017
Sean Kinread <dev@kinread.com>
2025-06-05 09:20:11 +1000

    revert: sprinkes, typography

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,5 @@
 export * from './components';
 export * from './hooks';
-export type {
-	Sprinkles as BaseStyleProps,
-	SprinklesResponsive as ResponsiveStyleProps,
-	SprinklesLegacyColours as LegacyColourStyleProps,
-} from './styles/sprinkles.css';
+export type { Sprinkles as StyleProps } from './styles/sprinkles.css';
 export { overdriveTokens as tokens } from './themes/theme.css';
 export { useId, arrayRingLookup } from './utils/index';
--- lib/styles/typography.css.ts
+++ lib/styles/typography.css.ts
@@ -1,50 +1,59 @@
-import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
+import { styleVariants } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 import { overdriveTokens as tokens } from '../themes/theme.css';
 
-export const typographyStyles = recipe({
-	base: {
-		selectors: {
-			'&::selection': {
-				color: tokens.typography.colour.white,
-				background: tokens.typography.colour.primary,
-			},
-		},
+const fontScale = {
+	...tokens.typography.size,
+	xxs: tokens.typography.size[1],
+	xs: tokens.typography.size[2],
+	sm: tokens.typography.size[3],
+	md: tokens.typography.size[4],
+	lg: tokens.typography.size[5],
+	xl: tokens.typography.size[6],
+	'2xl': tokens.typography.size[7],
+	'3xl': tokens.typography.size[8],
+	'4xl': tokens.typography.size[9],
+};
+
+const fontSizes = Object.entries(fontScale).reduce(
+	(obj, [key, value]) => {
+		obj[key] = { fontSize: value.fontSize };
+		return obj;
 	},
+	{} as Record<keyof typeof fontScale, { fontSize: string }>,
+);
+
+export const fontSize = styleVariants(fontSizes);
+export const fontWeight = styleVariants(
+	tokens.typography.fontWeight,
+	(weight) => ({
+		fontWeight: weight,
+	}),
+);
+
+/**
+ * A recipe for styling text from the typography scale, including line-height and font weight.
+ */
+export const styledFont = recipe({
+	base: [],
 	variants: {
-		breakWord: {
-			true: {
-				wordBreak: 'break-word',
-			},
-		},
-		noWrap: {
-			true: {
-				whiteSpace: 'nowrap',
-			},
-		},
-		transform: {
-			uppercase: {
-				textTransform: 'uppercase',
-			},
-			capitalize: {
-				textTransform: 'capitalize',
-			},
+		size: fontScale,
+		weight: fontWeight,
+		lineHeight: {
+			true: [],
 		},
 	},
+	defaultVariants: {
+		size: 'md',
+		lineHeight: true,
+	},
+	compoundVariants: [
+		{
+			variants: { lineHeight: false },
+			style: {
+				lineHeight: '1',
+			},
+		},
+	],
 });
-
-export type TypographyVariants = NonNullable<
-	RecipeVariants<typeof typographyStyles>
->;
-
-export interface TypographyStyles extends TypographyVariants {
-	/**
-	 * Prefer the `textWrap` prop on Box/boxStyles
-	 * @deprecated
-	 */
-	noWrap?: TypographyVariants['noWrap'];
-	/**
-	 * Applies CSS capitalisation
-	 */
-	transform?: TypographyVariants['transform'];
-}
