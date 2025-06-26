1c28b39125a2e31f08f3ac22386da8056008c5bb
Sean Kinread <dev@kinread.com>
2025-06-27 09:59:49 +1000

    Fix: componentStyles logic (#1187)
    * fix: critical componentStyle logic bug
    
    * test: unit test componentStyles
    
    * chore: changeset

--- lib/styles/componentStyles.ts
+++ lib/styles/componentStyles.ts
@@ -47,7 +47,12 @@ const hasBorderRelatedProp = (props: Sprinkles) =>
  * Returns the reset styles based on the `as` prop tag name passed in.
  * A wrapper around `resetVariants` recipe to allow any string to be passed in
  */
-export const resetStyles = (element: string, options?: ResetPropsWithoutAs) => {
+export const resetStyles = (
+	element: unknown,
+	options?: ResetPropsWithoutAs,
+) => {
+	if (typeof element !== 'string') return '';
+
 	const as = (
 		element in elementReset ? element : undefined
 	) as ResetVariantProps['as'];
@@ -68,20 +73,16 @@ export interface ComponentStylesProps extends Sprinkles {
  * Includes logic for adding border reset styles when any border props are used.
  *
  * @example <svg className={componentStyles({ as: 'svg', padding: '1', size: '4' })} ... />
- * @example <div className={componentStyles({ as: 'div', borderTopWidth: '1', borderTopColor: 'soft' })}
+ * @example <div className={componentStyles({ borderTopWidth: '1', borderTopColor: 'soft' })}
  * // ^ this will trigger the border reset ^
  */
 export const componentStyles = ({
 	as,
 	className,
 	...styleProps
-}: ComponentStylesProps) => {
-	if (typeof as !== 'string') return clsx(className);
-
-	const hasBorder = hasBorderRelatedProp(styleProps);
-	return clsx(
-		resetStyles(as, { hasBorder }),
+}: ComponentStylesProps) =>
+	clsx(
+		resetStyles(as, { hasBorder: hasBorderRelatedProp(styleProps) }),
 		sprinkles(styleProps),
 		className,
 	);
-};
