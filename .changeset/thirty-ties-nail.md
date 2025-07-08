4eda06c4554995d3961430b99bc907bf6f3bb07e
Sean Kinread <dev@kinread.com>
2025-07-08 13:49:21 +1000

    fix: useBoxStyles alias export

---
+++ .changeset/thirty-ties-nail.md
@@ -0,0 +1,5 @@
+---
+'@autoguru/overdrive': patch
+---
+
+Fix: missing `useBoxStyles` alias export
--- lib/components/Text/index.ts
+++ lib/components/Text/index.ts
@@ -1,2 +1,1 @@
 export { Text } from './Text';
-export { textStyles as useTextStyles } from '../../styles/typography';
