2bc6cce152114e5df46d59a2d89e4434c7714c48
Sean Kinread <dev@kinread.com>
2025-07-09 08:15:24 +1000

    Fix: Missing `useBoxStyles` export (#1199)
    * fix: useBoxStyles alias export
    
    * refactor: new NumberBubble layout to improve centering
    
    * chore: vertical translate unneeded

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,9 @@ export * from './hooks';
 export * from './styles';
 export { overdriveTokens as tokens } from './themes/theme.css';
 export { sprinkles as css } from './styles/sprinkles.css';
+export {
+	elementStyles as useBoxStyles,
+	textStyles as useTextStyles,
+	type ElementStylesProps as BoxStyleProps,
+} from './styles';
 export { useId, arrayRingLookup } from './utils/index';
