87eeac6250b0dcb07d8edcb3db0b6ffd11ad20fe
Sean Kinread <dev@kinread.com>
2025-07-21 14:34:17 +1000

    patch: Responsive text sizes (#1215)
    * feat: configure sprinkles for responsive text size
    
    * feat: adds a simple responsiveStyles helper for possible future use
    
    * test: snapshots
    
    * chore: changeset
    
    * feat: allow layer to be passed in to responsiveStyles function

---
+++ .changeset/rude-words-battle.md
@@ -0,0 +1,5 @@
+---
+'@autoguru/overdrive': patch
+---
+
+Responsive style props for text size are enabled in Text and Heading components.
--- lib/styles/layers.css.ts
+++ lib/styles/layers.css.ts
@@ -12,3 +12,18 @@ export const cssLayerTypography = globalLayer(
 export const cssLayerStyleprops = globalLayer('styleprops');
 export const cssLayerComponent = globalLayer('component');
 export const cssLayerUtil = globalLayer('util');
+
+/**
+ * Map of CSS layer names to their corresponding layer objects
+ */
+export const cssLayerMap = {
+	reset: cssLayerReset,
+	theme: cssLayerTheme,
+	colorset: cssColorSet,
+	typography: cssLayerTypography,
+	styleprops: cssLayerStyleprops,
+	component: cssLayerComponent,
+	util: cssLayerUtil,
+} as const;
+
+export type CSSLayers = keyof typeof cssLayerMap;
